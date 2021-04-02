e39036a023054e594f852faad0f224c0f06e3294
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-02 20:06:03 +0100

    text font set up (Montserrat 300)

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from "./App";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render((
     <BrowserRouter>
