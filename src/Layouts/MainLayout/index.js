77f796a6236c77daf457881e1b8f9d16a6bb616d
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-02 20:01:16 +0100

    layouts.MainLayout responsiveness

--- src/Layouts/MainLayout/index.js
+++ src/Layouts/MainLayout/index.js
@@ -1,13 +1,24 @@
 import React from "react";
 import { Outlet } from 'react-router-dom';
 
+import {
+    Col
+} from "react-bootstrap";
+
 import Navbar from "../../Components/Navbar";
 
 const MainLayout = () => {
     return (
         <div>
             <Navbar />
-            <Outlet />
+
+            <Col
+                lg={10}
+                xs={11}
+                className="mx-auto py-5"
+            >
+                <Outlet />
+            </Col>
         </div>
     );
 }
