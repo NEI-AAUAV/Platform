d8fc22ba105d4caf438795e94b5b0e4665b43055
Diogo Monteiro <56603629+CrustyMarsh@users.noreply.github.com>
2021-04-04 21:24:06 +0100

    pretty much done

--- src/Components/ImageCard/index.js
+++ src/Components/ImageCard/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import './index.css';
 
 const ImageCard = (props) => {
     /* Prop list:
@@ -8,15 +9,28 @@ const ImageCard = (props) => {
     **  text: plain text on bottom
     **  image: card image
     **  anchor: link associated with image and title
-    **  dark_mode (optional): set to true for dark color scheme
+    **  dark_mode (optional): set to a non-false value (e.g. "on" or True) to use a dark color scheme
     */
+
+    var color_class = "";
+    if (props.dark_mode) {
+        color_class = " dark"
+    }
+
     return(
-        <Card className="text-center mb-4">
-            <Card.Img variant="top img-fluid" src={props.image} href={props.anchor} />
-            <Card.Body>
+        <Card className={"text-center mb-5 image-card" + color_class}>
+            <a href={props.anchor}>
+            <Card.Img variant="top img-fluid" src={props.image} />
+            </a>
+
+            <div className={"plus-button" + color_class}>
+                <span><a href={props.anchor}>+</a></span>
+            </div>
+
+            <Card.Body className={color_class}>
                 {props.pre_title && <p1>{props.pre_title}</p1> }
-                <Card.Title href={props.anchor}>{props.title}</Card.Title>
-                <Card.Text>{props.text}</Card.Text>
+                <Card.Title><a href={props.anchor}>{props.title}</a></Card.Title>
+                <Card.Text><small>{props.text}</small></Card.Text>
             </Card.Body>
         </Card>
     );
--- src/Pages/Partners/index.js
+++ src/Pages/Partners/index.js
@@ -1,52 +1,56 @@
 import React from "react";
 import ImageCard from "../../Components/ImageCard"
-import { Col, Row, CardDeck } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap"
 
 // for testing
 const partners = [
     {
-        image: process.env.PUBLIC_URL + "/logo192.png",
+        image: process.env.PUBLIC_URL + "/testImage.jpg",
         name: "first partner",
         text: "lorem ipsum yabadabadaba doooo it'sa me Fred Flintstone of the stone age"
     },
     {
-        image: process.env.PUBLIC_URL + "/logo512.png",
+        image: process.env.PUBLIC_URL + "/testImage.jpg",
         name: "second partner",
         text: "lorem ipsum so he's finally here, performing for you. if you know the words, you can join in too! put your hands together if you wanna clap, as we take you through this monkey rap! huh! DK! Donkey Kong!"
     },
     {
-        image: process.env.PUBLIC_URL + "/logo192.png",
+        image: process.env.PUBLIC_URL + "/testImage.jpg",
         name: "third partner",
         text: "lorem ipsum hey vsauce michael here did you know that a square meter is also a rectangle meter"
     },
     {
-        image: process.env.PUBLIC_URL + "/logo512.png",
+        image: process.env.PUBLIC_URL + "/testImage.jpg",
         name: "fourth partner",
         text: "lorem ipsum ev no rvji orvireo vikv reij reivj erikvwvw  irvwei jwi rhvrrie vhjei kb"
     }
 ];
 
 const Partners = () => {
-    // tenho de ver como fazer o card deck responsive
-
     return (
-        <div>
-            <h1>Partners</h1>
+        <Container>
+            {
+            // TODO: this needs to be changed to the self-writing text thing
+            // also, background particles (that might be in the MainLayout?)
+            }
+            <h1 className="text-center">Parceiros</h1>
             
-            <CardDeck>
+            <Row>
                 {partners.map( partner => {
                     return(
-                        //<Col xl={6}>
+                        <Col md={6}>
                             <ImageCard
                                 image={partner.image}
                                 title={partner.name}
                                 text={partner.text}
+                                anchor="#"
+                                dark_mode="on"
                             ></ImageCard>
-                        //</Col>
+                        </Col>
                     );
                 })}
-            </CardDeck>
-        </div>
+            </Row>
+        </Container>
     );
 }
 
