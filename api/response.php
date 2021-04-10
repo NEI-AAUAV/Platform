19277ae23f1d7c903c7f6fb457fcdd5ec8a7e861
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-10 09:44:10 +0100

    api.response to avoid code duplication

---
+++ api/response.php
@@ -0,0 +1,33 @@
+<?php 
+
+    // This script gathers response related code, to avoid code duplication
+
+    /**
+     * This function returns an array as JSON
+     */
+    function response($st, $res) {
+        // Execute query
+        $st->execute();
+        
+        // Build JSON Response
+        if($st->rowCount() > 0){
+            $res = $st->fetchAll(PDO::FETCH_ASSOC);
+            $object = (object) ['data' => $res];
+            echo json_encode($object);
+        } else {
+            $object = (object) ['data' => []];
+            echo json_encode($object);
+        }
+    }
+
+    function errorResponse($message, $statusCode=400) {
+        // Build JSON
+        $object = (object) ['error' => $message];
+        echo json_encode($object);
+        // Set HTTP status
+        http_response_code($statusCode);
+        // Terminate script
+        exit();
+    }
+
+?>
