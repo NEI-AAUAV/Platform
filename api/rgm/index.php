296df5f1f53df113c21c8a365b18287ee69b29af
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-13 11:56:47 +0100

    api.* added error details to error response

--- api/rgm/index.php
+++ api/rgm/index.php
@@ -29,7 +29,7 @@
         // Return response
         response($st);
     } catch(Exception $e){
-        errorResponse('Ocorreu um erro inesperado.', 500);
+        errorResponse('Ocorreu um erro inesperado.', 500, $e);
     }
 
     // The connection is closed automatically when the script ends
