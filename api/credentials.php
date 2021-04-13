b022e01456f8972bed9c1a045e9bad1c07399cd6
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-13 21:40:47 +0100

    api.credentials update for development database

--- api/credentials.php
+++ api/credentials.php
@@ -1,7 +1,7 @@
 <?php
 
-    $servername = "mysql-hosting.ua.pt";
+    $servername = "gmatos.pt";
     $username = "aauav-nei-web";
-    $password = "@VsmlVJT^ajtKH3x$4Nx&F2R%MB%nakmBFdyWvm3QOFZXYtPUSngozy^XpY!gjjZ9!M&S3yG3D5CXNgYCBey*BhR4t38^W@Qy";
+    $password = "oAti009~";
 
 ?>
