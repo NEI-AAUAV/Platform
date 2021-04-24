b3ee4e1fe51c260ccecc53e00a6c444b68941810
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-24 16:16:39 +0100

    api.history (table added to database)

---
+++ api/changes.sql
@@ -0,0 +1,15 @@
+-- Changes to data model
+
+-- Create table for History
+CREATE TABLE history (
+    moment DATE PRIMARY KEY,
+    title VARCHAR(120),
+    body TEXT,
+    image VARCHAR(255)
+)
+
+INSERT INTO history (moment, title, image, body) VALUES 
+    ("2019-06-30", "Candidatura ENEI 2020", "/images/history/20190630.png", "Entrega de uma candidatura conjunta (NEI+NEECT+AETTUA) para a organização do Encontro Nacional de Estudantes de Informática 2019. Esta candidatura teve a concorrência de uma candidatura conjunta da CESIUM, constituída por alunos da Universidade do Minho, que acabaram por ser a candidatura vencedora."),
+    ("2019-06-12", "2º Lugar Futsal", "/images/history/20190612.jpg", "Num jogo em que se fizeram das tripas coração, o NEI defrontou a equipa de EGI num jogo que veio a perder, foi um jogo bastante disputado, contudo, acabou por ganhar EGI remetendo o NEI para o 2º lugar."),
+    ("2018-04-30", "Elaboração de Candidatura para o Encontro Nacional de Estudantes de Informática 2019", "/images/history/20180430.png", "Entrega de uma candidatura conjunta (NEI+AETTUA) para a organização do Encontro Nacional de Estudantes de Informática 2019. Esta candidatura teve a concorrência de uma candidatura conjunta do ISCAC Junior Solutions e Junisec, constituída por alunos do Politécnico de Coimbra, que acabaram por ser a candidatura vencedora.")
+;
