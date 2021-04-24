764895c54e65085b273be4aa943a2491b51a8510
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-24 17:35:48 +0100

    api.seniors (table added to database)

--- api/changes.sql
+++ api/changes.sql
@@ -13,3 +13,34 @@ INSERT INTO history (moment, title, image, body) VALUES
     ("2019-06-12", "2º Lugar Futsal", "/images/history/20190612.jpg", "Num jogo em que se fizeram das tripas coração, o NEI defrontou a equipa de EGI num jogo que veio a perder, foi um jogo bastante disputado, contudo, acabou por ganhar EGI remetendo o NEI para o 2º lugar."),
     ("2018-04-30", "Elaboração de Candidatura para o Encontro Nacional de Estudantes de Informática 2019", "/images/history/20180430.png", "Entrega de uma candidatura conjunta (NEI+AETTUA) para a organização do Encontro Nacional de Estudantes de Informática 2019. Esta candidatura teve a concorrência de uma candidatura conjunta do ISCAC Junior Solutions e Junisec, constituída por alunos do Politécnico de Coimbra, que acabaram por ser a candidatura vencedora.")
 ;
+
+-- Create table for seniors
+CREATE TABLE seniors (
+    year INT, 
+    course VARCHAR(3),
+    image VARCHAR(255),
+    PRIMARY KEY (year, course)
+)
+
+INSERT INTO seniors (year, course, image) VALUES
+    (2020, "LEI", "/images/seniors/LEI/2020.jpg"),
+    (2020, "MEI", "/images/seniors/MEI/2020.jpg")
+;
+
+CREATE TABLE seniors_students (
+    year INT,
+    course VARCHAR(3),
+    userId INT,
+    FOREIGN KEY (year, course) REFERENCES seniors(year, course),
+    FOREIGN KEY (userId) REFERENCES users(id),
+    PRIMARY KEY (year, course, userId)
+)
+
+INSERT INTO seniors_students (course, year, userId) VALUES
+    ("LEI", 2020, 873),
+    ("LEI", 2020, 900),
+    ("LEI", 2020, 1545),
+    ("LEI", 2020, 1362),
+    ("LEI", 2020, 879),
+    ("MEI", 2020, 1059)
+;
---
+++ api/seniors/index.php
@@ -0,0 +1,63 @@
+<?php 
+    // Load database connection from start.php file
+    require_once("../start.php");
+    // Load response assistant script
+    require_once("../response.php");
+?>
+<?php
+    
+    // Get url parameter and validate it 
+    $course = $_GET["course"];
+    $year = $_GET["year"];
+    $validOptions = $conn->query("SELECT DISTINCT course, year FROM seniors")->fetchAll(PDO::FETCH_ASSOC);
+    $validCourse = false;
+    $validYear = false;
+    foreach($validOptions as $op) {
+        if ($op['course']==$course) {
+            $validCourse = true;
+        }
+        if ($op['year']==$year) {
+            $validYear = true;
+        }
+    }
+    if(!$validCourse and is_null($_GET["course"])) {
+        errorResponse('Parâmetro "course" em falta!');
+    } else if (!$validCourse) {
+        errorResponse('Parâmetro "course" inválido!');
+    } else if(!$validYear and is_null($_GET["year"])) {
+        errorResponse('Parâmetro "year" em falta!');
+    } else if (!$validYear) {
+        errorResponse('Parâmetro "year" inválido!');
+    }
+
+    // Make query to the database
+    $query_getContent = "SELECT image FROM seniors WHERE course=:course AND year=:year";
+    $query2_getContent = "SELECT users.name FROM seniors_students INNER JOIN users ON users.id=seniors_students.userId WHERE seniors_students.course=:course AND seniors_students.year=:year ORDER BY users.name";
+
+    try{
+        // Query 1
+        $st = $conn->prepare($query_getContent);
+        $st->bindParam(':course', $course);
+        $st->bindParam(':year', $year);
+        // Query 2
+        $st2 = $conn->prepare($query2_getContent);
+        $st2->bindParam(':course', $course);
+        $st2->bindParam(':year', $year);
+        // Execute query
+        $st->execute();
+        $st2->execute();
+        // Return response
+        // Build JSON Response
+        $res = $st->fetchAll(PDO::FETCH_ASSOC);
+        $res2 = $st2->fetchAll(PDO::FETCH_ASSOC);
+        // Add res2 to res
+        $res[0]['students'] = $res2;
+        $object = (object) ['data' => $res];
+        echo json_encode($object);
+        exit();
+    } catch(Exception $e){
+        errorResponse('Ocorreu um erro inesperado.', 500, $e);
+    }
+
+    // The connection is closed automatically when the script ends
+?>
