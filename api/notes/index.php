7117a36b66382427b916d6161c6d27831612b9cb
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-04-25 15:39:53 +0100

    api.notes.validators to avoid code duplication

--- api/notes/index.php
+++ api/notes/index.php
@@ -5,6 +5,8 @@
     require_once("../response.php");
     // Load pagination assistant script
     require_once("../pagination.php");
+    // Load notes validators
+    require_once("./validators.php");
 ?>
 <?php
 
@@ -22,107 +24,21 @@
     );
 
     // Parameter validation
-    if(count($categories)>0) {
-        $validOptions = ['summary', 'tests', 'bibliography', 'slides', 'exercises', 'projects', 'notebook'];
-        $valid = true;
-        foreach($categories as $category) {
-            $validCat = false;
-            foreach($validOptions as $op) {
-                if ($op==$category) {
-                    $validCat = true;
-                }
-            }
-            $valid = $valid && $validCat;
-        }
-        if(!$valid) {
-            errorResponse('Categoria inválida!');
-        }
-    }
-
-    if(!is_null($teacher)) {
-        $validOptions = $conn->query("SELECT id FROM notes_teachers")->fetchAll(PDO::FETCH_ASSOC);
-        $valid = false;
-        foreach($validOptions as $op) {
-            if ($op['id']==$teacher) {
-                $valid = true;
-            }
-        }
-        if (!$valid) {
-            errorResponse('Parâmetro "teacher" inválido!');
-        }
-    }
-
-    if(!is_null($student)) {
-        $validOptions = $conn->query("SELECT DISTINCT author FROM notes")->fetchAll(PDO::FETCH_ASSOC);
-        $valid = false;
-        foreach($validOptions as $op) {
-            if ($op['author']==$student) {
-                $valid = true;
-            }
-        }
-        if (!$valid) {
-            errorResponse('Parâmetro "student" inválido!');
-        }
-    }
-
-    if(!is_null($subject)) {
-        $validOptions = $conn->query("SELECT DISTINCT subject FROM notes")->fetchAll(PDO::FETCH_ASSOC);
-        $valid = false;
-        foreach($validOptions as $op) {
-            if ($op['subject']==$subject) {
-                $valid = true;
-            }
-        }
-        if (!$valid) {
-            errorResponse('Parâmetro "subject" inválido!');
-        }
-    }
-
-    if(!is_null($schoolYear)) {
-        $validOptions = $conn->query("SELECT DISTINCT schoolYear FROM notes")->fetchAll(PDO::FETCH_ASSOC);
-        $valid = false;
-        foreach($validOptions as $op) {
-            if ($op['schoolYear']==$schoolYear) {
-                $valid = true;
-            }
-        }
-        if (!$valid) {
-            errorResponse('Parâmetro "schoolYear" inválido!');
-        }
-    }
+    $categoriesQuery = validateCategories($categories, $conn);
+    $studentQuery = validateStudent($student, $conn);
+    $subjectQuery = validateSubject($subject, $conn);
+    $schoolYearQuery = validateSchoolYear($schoolYear, $conn);
+    $teacherQuery = validateTeacher($teacher, $conn);
 
     // Get news list (with or without category filtering)
     $query_getContent = "SELECT notes.id, notes.name, notes_subjects.short AS subjectShort, notes_subjects.name AS subjectName, notes.summary, notes.tests, notes.bibliography, notes.slides, notes.exercises, notes.projects, notes.notebook FROM notes INNER JOIN notes_subjects ON notes.subject=notes_subjects.paco_code INNER JOIN notes_schoolyear ON notes.schoolYear=notes_schoolyear.id";
 
-    $query_getContent.=" WHERE";
-
-    if(count($categories)>0) {
-        $query_getContent.=" (";    
-        $counter = 0;
-        foreach($categories as $category) {
-            $query_getContent.= $category . "=1";
-            $counter = $counter + 1; 
-            if($counter < count($categories)) {
-                $query_getContent.=" OR ";
-            }   
-        }
-        $query_getContent.=")";
-    } else {
-        $query_getContent.=" 1";
-    }
-
-    if(!is_null($teacher)) {
-        $query_getContent.=" AND notes.teacher=:teacher";
-    }
-    if(!is_null($student)) {
-        $query_getContent.=" AND notes.author=:student";
-    }
-    if(!is_null($subject)) {
-        $query_getContent.=" AND notes.subject=:subject";
-    }
-    if(!is_null($schoolYear)) {
-        $query_getContent.=" AND notes.schoolYear=:schoolYear";
-    }
+    $query_getContent.= " WHERE";
+    $query_getContent.= $categoriesQuery;
+    $query_getContent.= $teacherQuery;
+    $query_getContent.= $studentQuery;
+    $query_getContent.= $subjectQuery;
+    $query_getContent.= $schoolYearQuery;
 
     $query_getContent.= " ORDER BY notes_schoolyear.yearEnd DESC, notes.createdAt DESC";
 
@@ -130,16 +46,16 @@
     try{
         $st = $conn->prepare($query_getContent);
         // Bind parameters to query
-        if(!is_null($teacher)) {
+        if(!is_null($teacherQuery)) {
             $st->bindParam(":teacher", $teacher);
         }
-        if(!is_null($student)) {
+        if(!is_null($studentQuery)) {
             $st->bindParam(":student", $student);
         }
-        if(!is_null($subject)) {
+        if(!is_null($subjectQuery)) {
             $st->bindParam(":subject", $subject);
         }
-        if(!is_null($schoolYear)) {
+        if(!is_null($schoolYearQuery)) {
             $st->bindParam(":schoolYear", $schoolYear);
         }
         // Return paginated results
