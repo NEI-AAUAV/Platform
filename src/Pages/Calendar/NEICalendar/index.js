fb3bbfe7dafefa71a843b32ed0db5cc7c18c1c8d
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-22 16:37:04 +0100

    calendar getting events from NEI Google Calendar

--- src/Pages/Calendar/NEICalendar/index.js
+++ src/Pages/Calendar/NEICalendar/index.js
@@ -1,11 +1,10 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {
     Calendar as ReactCalendar,
     Views,
     momentLocalizer
 } from 'react-big-calendar';
 import moment from 'moment';
-import events from '../events';
 import "./index.css";
 
 
@@ -22,15 +21,38 @@ const localizer = momentLocalizer(moment);
 
 
 const NEICalendar = () => {
+    const [events, setEvents] = useState([]);
+
+    useEffect(() => {
+        console.log("STARTED REQUEST");
+        fetch('https://www.googleapis.com/calendar/v3/calendars/7m2mlm7k1huomjeaa45gbhog0k%40group.calendar.google.com/events?key=AIzaSyDnT8fO6ARjx3OxMJCimhenNDLTkGuOmjE&timeMin=2021-04-25T00%3A00%3A00%2B01%3A00&timeMax=2021-06-06T00%3A00%3A00%2B01%3A00&singleEvents=true&maxResults=9999')
+            .then(res => res.json())
+            .then(json => {
+                console.log(json['items']);
+                let apiEvents = [];
+                json['items'].forEach(e => {
+                    apiEvents.push({
+                        'id': e['id'],
+                        'title': e['summary'],
+                        'start': new Date(e['start']['date']),
+                        'end': new Date(e['end']['date'])
+                    });
+                });
+                console.log(apiEvents);
+                setEvents(apiEvents);
+            });
+    }, []);
+
     return (
+        events.length>0
+        &&
         <ReactCalendar
             className="col-12 vh-100"
             events={events}
             views={allViews}
             step={60}
             showMultiDayTimes
-            max={new Date(2021, 12, 31)}
-            defaultDate={new Date(2015, 3, 1)}
+            defaultDate={new Date()}
             components={{
                 timeSlotWrapper: ColoredDateCellWrapper,
             }}
--- src/Pages/Calendar/index.js
+++ src/Pages/Calendar/index.js
@@ -1,10 +1,7 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
 import NEICalendar from "./NEICalendar";
 
-
 const Calendar = () => {
-
 
     return (
         <div>
