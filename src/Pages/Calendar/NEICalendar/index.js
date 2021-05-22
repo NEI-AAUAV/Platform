694c1b11f6ebe71c4dfc6660b19902629baca8d2
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-22 19:48:55 +0100

    calendar events colors varying according to event type

--- src/Pages/Calendar/NEICalendar/index.js
+++ src/Pages/Calendar/NEICalendar/index.js
@@ -11,11 +11,28 @@ import "./index.css";
 let allViews = Object.keys(Views).map(k => Views[k]);
 const localizer = momentLocalizer(moment);
 
-const ColoredDateCellWrapper = ({ children }) =>
-    React.cloneElement(React.Children.only(children), {
+// Colorize events acoording to type
+const eventStyleGetter = (event, start, end, isSelected) => {
+    // 
+    let color = "#147a26";
+    if (event.['title'].indexOf("MEI")>=0) {
+        color = "#015a65";
+    } else if (event.['title'].indexOf("[3A]")>=0) {
+        color = "#018798";
+    } else if (event.['title'].indexOf("[2A]")>=0) {
+        color = "#01abc0";
+    } else if (event.['title'].indexOf("[1A]")>=0) {
+        color = "#01cae4";
+    } else if (["FERIADO", "Época de", "Férias"].some(term => event['title'].toLowerCase().indexOf(term.toLowerCase())>=0)) {
+        color = "#FFA200";
+    }
+
+    return {
         style: {
-            backgroundColor: 'lightblue',
-        },
+            'backgroundColor': color
+        }
+    }
+}
 
 // COMPONENT
 const NEICalendar = () => {
@@ -55,11 +72,27 @@ const NEICalendar = () => {
         }
     }, [calendarSince, calendarTo]);
 
-const localizer = momentLocalizer(moment);
+    // On navigate, update next and prev moments and recall API
+    const timespanChanged = (date) => {
+        let next = new Date(date);
+        let prev = new Date(date);
 
+        if (date.getMonth()==11) {
+            next.setMonth(0);
+            next.setYear(date.getFullYear()+1);
+            prev.setMonth(date.getMonth()-1);
+        } else if (date.getMonth()==0) {
+            next.setMonth(date.getMonth()+1);
+            prev.setMonth(11);
+            prev.setYear(date.getFullYear()-1);
+        } else {
+            next.setMonth(date.getMonth()+1);
+            prev.setMonth(date.getMonth()-1);
+        }
 
-const NEICalendar = () => {
-    const [events, setEvents] = useState([]);
+        setCalendarSince(prev);
+        setCalendarTo(next);
+    }
 
     useEffect(() => {
         console.log("STARTED REQUEST");
@@ -91,10 +124,9 @@ const NEICalendar = () => {
             step={60}
             showMultiDayTimes
             defaultDate={new Date()}
-            components={{
-                timeSlotWrapper: ColoredDateCellWrapper,
-            }}
+            eventPropGetter={eventStyleGetter}
             localizer={localizer}
+            onNavigate={(date) => timespanChanged(date)}
         />
     );
 }
