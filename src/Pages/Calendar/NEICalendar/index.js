23349c4ff9c3782257fbb6e820560660fe5c1b71
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-23 08:25:02 +0100

    tooltip on event over with start and end hour(/day/month if relevant)

--- src/Pages/Calendar/NEICalendar/index.js
+++ src/Pages/Calendar/NEICalendar/index.js
@@ -30,6 +30,51 @@ const eventStyleGetter = (event, start, end, isSelected) => {
     }
 }
 
+ // Tooltip acessor
+// This method returns the text tooltip given an event 
+const tooltipAcessor = (e) => {    
+    if (e['allDay']) {
+        return "All day";
+    }
+    // If not all day, compute text for time span
+    if ((Math.abs(e['end']-e['start'])/(1000 * 60 * 60))<24) {
+        // Same day (less than 24 hours diff)
+        return `From ${zeroPad(e['start'].getHours()+1, 2)}:${zeroPad(e['start'].getMinutes(), 2)} to ${zeroPad(e['end'].getHours()+1, 2)}:${zeroPad(e['end'].getMinutes(), 2)}`;
+    } else {
+        // Else, different days
+        if (e['start'].getMonth() == e['end'].getMonth()) {
+            // On same month
+            return `From ${dayString(e['start'].getDate())} at ${zeroPad(e['start'].getHours()+1, 2)}:${zeroPad(e['start'].getMinutes(), 2)} to ${dayString(e['end'].getDate())} at ${zeroPad(e['end'].getHours()+1, 2)}:${zeroPad(e['end'].getMinutes(), 2)}`;
+        } else {
+            // On different months
+            return `From ${months[e['start'].getMonth()]} the ${dayString(e['start'].getDate())} at ${zeroPad(e['start'].getHours()+1, 2)}:${zeroPad(e['start'].getMinutes(), 2)} to ${months[e['end'].getMonth()]} the ${dayString(e['end'].getDate())} at ${zeroPad(e['end'].getHours()+1, 2)}:${zeroPad(e['end'].getMinutes(), 2)}`;
+        }
+    }
+    return "";
+}
+
+// Add leading zeros to numbers (Example: (3, 2) => 03)
+const zeroPad = (num, places) => String(num).padStart(places, '0');
+
+// Convert day to string (Xst/Xnd/Xrd/Xth) (Example: (1)=>"1st")
+const dayString = (day) => {
+    let append = "th";
+    switch(day) {
+        case 1:
+            append = "st";
+            break;
+        case 2:
+            append = "nd";
+            break;
+        case 3:
+            append = "rd";
+            break;
+    }
+    return `${day}${append}`;
+}
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
 // COMPONENT
 const NEICalendar = () => {
     
@@ -111,6 +156,7 @@ const NEICalendar = () => {
             eventPropGetter={eventStyleGetter}
             localizer={momentLocalizer(moment)}
             onNavigate={(date) => timespanChanged(date)}
+            tooltipAccessor={(event) => tooltipAcessor(event)}
         />
     );
 }
