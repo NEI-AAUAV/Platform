b60324d536d555408d0e62df21465613e2b3ba7f
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-05-29 10:38:49 +0100

    layouts main layout background animation set up

--- src/Layouts/MainLayout/index.js
+++ src/Layouts/MainLayout/index.js
@@ -1,25 +1,38 @@
 import React from "react";
 import { Outlet } from 'react-router-dom';
+import Particles from "react-tsparticles";
 
 import {
     Col
 } from "react-bootstrap";
 
 import Navbar from "../../Components/Navbar";
+import config from "./config";
 
 const MainLayout = () => {
-    return (
-        <div>
-            <Navbar />
 
-            <Col
-                lg={10}
-                xs={11}
-                className="mx-auto py-5"
-            >
-                <Outlet />
-            </Col>
-        </div>
+    return (
+        <>
+            <Particles
+                id="tsparticles"
+                init={() => console.log("INIT")}
+                loaded={() => console.log("LOADED")}
+                options={config}
+                className="position-absolute"
+            />
+            <div>
+                <Navbar />
+
+
+                <Col
+                    lg={10}
+                    xs={11}
+                    className="mx-auto py-5"
+                >
+                    <Outlet />
+                </Col>
+            </div>
+        </>
     );
 }
 
