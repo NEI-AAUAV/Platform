e17984e4d635327ced04b3ecbb3af16aee720ab8
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-06-02 15:23:08 +0100

    CleanLayout set up

--- src/Layouts/CleanLayout/index.js
+++ src/Layouts/CleanLayout/index.js
@@ -10,7 +10,7 @@ import Navbar from "../../Components/Navbar";
 import Footer from "../../Components/Footer";
 import config from "../backgroundconfig";
 
-const MainLayout = () => {
+const CleanLayout = () => {
 
     return (
         <>
@@ -23,20 +23,11 @@ const MainLayout = () => {
             />
             <div className="pt-5">
                 <Navbar />
-
-                <Col
-                    xs={11}
-                    sm={10}
-                    xxl={9}
-                    className="mx-auto py-5"
-                >
-                    <Outlet />
-                </Col>
-
+                <Outlet />
                 <Footer />
             </div>
         </>
     );
 }
 
-export default MainLayout;
+export default CleanLayout;
