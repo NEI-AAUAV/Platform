e6a40492a0f87737fe38f12ceb06b700e09c82a0
Gonçalo Matos <gmatos.ferreira@ua.pt>
2021-06-13 22:59:55 +0100

    api credentials for nei server

--- api/credentials.php
+++ api/credentials.php
@@ -1,7 +1,8 @@
 <?php
 
-    $servername = "gmatos.pt";
+    $servername = "mysql-hosting.ua.pt";
     $username = "aauav-nei-web";
-    $password = "oAti009~";
+    $password = "@VsmlVJT^ajtKH3x$4Nx&F2R%MB%nakmBFdyWvm3QOFZXYtPUSngozy^XpY!gjjZ9!M&S3yG3D5CXNgYCBey*BhR4t38^W@Qy";
+
 
 ?>
