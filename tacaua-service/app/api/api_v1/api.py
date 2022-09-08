f01a05481fcee8f4f8518097752323515613df4e
leand12 <leandrosilva12@ua.pt>
2022-09-08 07:10:48 +0100

    remove prev setup

--- tacaua-service/app/api/api_v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-from app.api.api_v1.endpoints import tacaua_game
-
-api_router = APIRouter()
-api_router.include_router(tacaua_game.router, prefix="/tacaua/games", tags=["Taça UA"])
-
