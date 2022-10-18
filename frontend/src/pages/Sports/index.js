1c318be8ae0733be6c445fb6fdf698dfb24e3e62
Dani Figueiredo <danifgrd8@gmail.com>
2022-10-18 17:03:21 +0100

    Deti Hall Component

--- frontend/src/pages/Sports/index.js
+++ frontend/src/pages/Sports/index.js
@@ -3,8 +3,8 @@ import Carousel from "react-bootstrap/Carousel";
 import { Row, Col } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 import TextList from "../../components/TextList";
-import Tabs from "../../components/Tabs/index.js";
-import Game from './Game';
+/* import Tabs from "../../components/Tabs/index.js"; */
+import Game from "./Game";
 import img1 from "./img/unknown.png";
 import img2 from "./img/unknown2.png";
 import img3 from "./img/unknown3.png";
@@ -13,7 +13,8 @@ import equipa from "./img/equipa.jpg";
 import Typist from "react-typist";
 
 import "./index.css";
-import SportTable from "./SportTable";
+/* import SportTable from "./SportTable"; */
+import DetiHall from "./DetiHall";
 
 const animationBase = parseFloat(process.env.REACT_APP_ANIMATION_BASE);
 const animationIncrement = parseFloat(
@@ -23,9 +24,9 @@ const animationIncrement = parseFloat(
 const Sports = () => {
   const [tabIndicator, setTabIndicator] = useState("Andebol");
   const [tabIndicatorSex, setTabIndicatorSex] = useState("Masculino");
-  const [img, setImg] = useState(null);
-  const [anos, setAnos] = useState([]);
-  const [selectedYear, setSelectedYear] = useState();
+  /* const [img, setImg] = useState(null); */
+  const [anos /* , setAnos */] = useState([]);
+  /* const [selectedYear, setSelectedYear] = useState(); */
   const [data, setData] = useState([]);
 
   /*setImg(<Image
@@ -39,9 +40,9 @@ const Sports = () => {
 
   useEffect(() => {
     fetch(process.env.REACT_APP_API + "/sports/modalities")
-      .then(response => response.json())
+      .then((response) => response.json())
       .then((response) => {
-        if ('data' in response) {
+        if ("data" in response) {
           setData(response["data"]);
           //console.log(data);
         }
@@ -69,20 +70,19 @@ const Sports = () => {
     setTabIndicator(value);
   }
 
-  function loadTab() {
+  /* function loadTab() {
     let result1 = [];
-    let result2 = [];
+    //let result2 = [];
     let bothGenders = false;
     let arr = [];
-    let info = new Map;
+    let info = new Map();
 
     for (let i = 0; i < data.length; i++) {
       const map = new Map(Object.entries(data[i]));
       if (arr.includes(map.get("name"))) {
         bothGenders = true;
         info.set(map.get("name"), true);
-      }
-      else {
+      } else {
         arr.push(map.get("name"));
         bothGenders = false;
       }
@@ -90,42 +90,40 @@ const Sports = () => {
         if (tabIndicator === map.get("name"))
           result1.push(<li class="act">{map.get("name")}</li>);
         else
-          result1.push(<li onClick={() => changeTab(map.get("name"))}>{map.get("name")}</li>);
+          result1.push(
+            <li onClick={() => changeTab(map.get("name"))}>
+              {map.get("name")}
+            </li>
+          );
       }
     }
 
     window.info = info;
 
-    return (
-      <>
-        {result1}
-      </>
-    );
+    return <>{result1}</>;
   }
 
   function loadGenderTab() {
     let result = [];
 
     window.info.forEach((value, key) => {
-      if (value && key == tabIndicator) {
+      if (value && key === tabIndicator) {
         if (tabIndicatorSex === "Masculino") {
           result.push(<li class="act">Masculino</li>);
-          result.push(<li onClick={() => setTabIndicatorSex("Feminino")}>Feminino</li>);
-        }
-        else {
-          result.push(<li onClick={() => setTabIndicatorSex("Masculino")}>Masculino</li>);
+          result.push(
+            <li onClick={() => setTabIndicatorSex("Feminino")}>Feminino</li>
+          );
+        } else {
+          result.push(
+            <li onClick={() => setTabIndicatorSex("Masculino")}>Masculino</li>
+          );
           result.push(<li class="act">Feminino</li>);
         }
       }
     });
 
-    return (
-      <>
-        {result}
-      </>
-    )
-
-  }
+    return <>{result}</>;
+  } */
 
   return (
     <>
@@ -172,22 +170,17 @@ const Sports = () => {
         </Carousel>
       </div>
       {anos}
-      <div style={{ marginTop: "1rem" }}>
+      <div style={{ marginTop: "5rem" }}>
         <h3 className="mb-5 text-center slideUpFade">Modalidades</h3>
       </div>
-      <div class="lista">
-        <ul className="slideUpFade">
-          {loadTab()}
-        </ul>
+      {/*  <div class="lista">
+        <ul className="slideUpFade">{loadTab()}</ul>
       </div>
-
       <div class="gender-list">
-        <ul className="slideUpFade">
-          {loadGenderTab()}
-        </ul>
-      </div>
+        <ul className="slideUpFade">{loadGenderTab()}</ul>
+      </div> */}
 
-      <div className="d-flex flex-column flex-wrap team-wrapper">
+      <div className="d-flex flex-column flex-wrap team-wrapper" y>
         <Row>
           <Image
             src={equipa}
@@ -267,7 +260,7 @@ const Sports = () => {
         </Row>
 
         <Row className="games-section">
-          <SportTable />
+          <DetiHall />
         </Row>
       </div>
     </>
