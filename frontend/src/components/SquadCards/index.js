554d321bfea47bc8bfb62029605570fca0486a31
Diana Miranda <dianarrmiranda@ua.pt>
2022-10-23 18:25:59 +0100

    Team

--- frontend/src/components/SquadCards/index.js
+++ frontend/src/components/SquadCards/index.js
@@ -1,45 +1,24 @@
-import React from "react";
-import { Card } from "react-bootstrap";
-import './index.css';
+import React from 'react';
 
-const SquadCard = (props) => {
-  /* Prop list:
-  **  preTitle (optional): plain text on top
-  **  title: large colored anchor text in middle
-  **  text: plain text on bottom
-  **  image: card image
-  **  anchor: link associated with image and title
-  **  darkMode (optional): set to a non-false value (e.g. "on" or True) to use a dark color scheme
-  **  animKey (optional): integer that increases delay on entrance animation
-  */
 
-  var color_class = "";
-  if (props.darkMode) {
-    color_class = " dark"
-  }
+import Image from 'react-bootstrap/Image';
+import { Container, Col } from 'react-bootstrap';
 
-  return (
-    <Card
-      className={"text-center img-card" + color_class}
-      
-    >
-      {props.image && (
-        <a href={props.anchor} target="_blank" rel="noreferrer noopener">
-          <Card.Img variant="top img-fluid" src={props.image} />
-        </a>
-      )}
+import "./index.css";
 
-      <Card.Body className={color_class}>
-        {props.preTitle && <p>{props.preTitle}</p>}
-        <Card.Title>
-          <a href={props.anchor} target="_blank" rel="noreferrer noopener">{props.title}</a>
-        </Card.Title>
-        <Card.Text>
-          <small>{props.text}</small>
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+
+
+const Player = () => {
+    return (
+        <div>
+            { 
+            <Col className="mx-auto" style={{ marginBottom: 40}}>
+                <Image className={"imgTeam"} src={"https://i0.wp.com/techwek.com/wp-content/uploads/2020/12/Imagem-para-perfil.jpg?resize=512%2C473&ssl=1"} style={{ borderRadius:20, width: 200, marginBottom: 20}}/>
+                <h3>Marco António</h3>
+            </Col>
+            }
+        </div>
+    )
 }
 
-export default SquadCard;
+export default Player;
--- frontend/src/pages/Sports/SportTeam/index.js
+++ frontend/src/pages/Sports/SportTeam/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 import equipa from "../img/equipa.jpg";
-import { Row, Col } from "react-bootstrap";
+import { Row } from "react-bootstrap";
 import Image from "react-bootstrap/Image";
 
-import SquadCard from "../../../components/SquadCards"
-
-
 import "./index.css";
+import Player from "components/SquadCards";
 
 const animationBase = parseFloat(process.env.REACT_APP_ANIMATION_BASE);
 const animationIncrement = parseFloat(
@@ -18,33 +16,28 @@ const SportTeam = () => {
 
   return (
     <> 
-        <div>
-            <Row>
-            <Image
-                src={equipa}
-                rounded
-                fluid
-                className="slideUpFade"
-                style={{
-                animationDelay: animationBase + animationIncrement * 0 + "s",
-                marginBottom: 50,
-                marginTop: 50,
-                }}
-            ></Image>
-            </Row> 
-        </div>
-        <div style={{ marginTop: "1rem" }}>
-            <h2 className="mb-5 slideUpFade">Plantel</h2>
-        </div>
-        <div>
-        <Col lg={2} md={8} sm={12}>
-            <SquadCard
-                image={equipa}
-                title="Marco António"
-            ></SquadCard>
-        </Col>
-        </div>
+      <div>
+        <Row>
+          <Image
+            src={equipa}
+            rounded
+            fluid
+            className="slideUpFade"
+            style={{
+            animationDelay: animationBase + animationIncrement * 0 + "s",
+            marginBottom: 50,
+            marginTop: 50,
+            }}
+          ></Image>
+        </Row> 
+      </div>
 
+      <div style={{ marginTop: "1rem" }}>  
+        <h2 className="mb-5 slideUpFade">Plantel</h2>
+      </div>
+      <div className="d-flex flex-column flex-wrap">
+        <Player/>
+      </div>
     </>
   );
 };
