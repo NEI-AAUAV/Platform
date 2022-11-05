81ed6ecbd5be391837855ccf3361cd9982c7964f
Pedro Monteiro <pmapm@ua.pt>
2022-11-05 00:18:20 +0000

    change games card display

--- frontend/src/pages/Sports/index.js
+++ frontend/src/pages/Sports/index.js
@@ -2,15 +2,34 @@ import React, { useState, useEffect } from "react";
 
 import Typist from "react-typist";
 import SideBar from "./components/SideBar";
-import TopBar from "./components/TopBar";
 import { Row, Col } from "react-bootstrap";
 import Game from './components/Game';
+import Dropdown from 'react-bootstrap/Dropdown';
+import img from './components/football.png';
 
 import "./index.css";
 import GameFilter from "./components/GameFilter";
 
 
 const Sports = () => {
+
+  const [active, setActive] = useState("games");
+
+  const selectChange = (value) => {
+    switch (value) {
+      case "games":
+        setActive("games");
+        break;
+      case "classification":
+        setActive("classification");
+        break;
+      case "team":
+        setActive("team");
+        break;
+      default:
+        return;
+    }
+  };
 
   return (
     <>
@@ -22,67 +41,77 @@ const Sports = () => {
       <div className="bars">
         <SideBar />
         <div className="games-and-top-bar">
-          <TopBar />
-          <div className="games">
-            <div className="games-filters">
-              <GameFilter text="Fase de Grupos - 1ª Div" />
-              <GameFilter text="Fase de Grupos - 2ª Div" />
-              <GameFilter text="Playoffs" />
+          <div className='top-bar'>
+            <div className='top-bar-items'>
+              <div className='top-bar-image-wrapper'>
+                <img src={img}></img>
+              </div>
+              <div className='top-bar-items-header'>
+                <h3>Futsal Masculino</h3>
+                <Dropdown className='top-bar-items-header-dropdown' style={{ outline: 'none' }}>
+                  <Dropdown.Toggle id="dropdown-basic" style={{ border: 'none', background: 'none', color: '#000' }}>
+                    2019
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu>
+                    <Dropdown.Item>2019</Dropdown.Item>
+                    <Dropdown.Item>2020</Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
             </div>
-            <h3 className="games-header">Grupo A</h3>
-            <div className="games-list">
-              <Row className="row-game">
-                <Col lg={4}>
-                  <Game gameDay="Jornada 1" data="31/02" result="3 - 0" />
-                </Col>
-                <Col lg={4}>
-                  <Game gameDay="Jornada 2" data="31/02" result="3 - 0"/>
-                </Col>
-                <Col lg={4}>
-                  <Game gameDay="Jornada 3" data="31/02" result="VS"/>
-                </Col>
-              </Row>
-              <Row className="row-game">
-                <Col lg={4}>
-                  <Game gameDay="Jornada 4" data="31/02" result="VS" />
-                </Col>
-                <Col lg={4}>
-                  <Game gameDay="Jornada 5" data="31/02" result="VS" />
-                </Col>
-                <Col lg={4}>
-                  <Game gameDay="Jornada 6" data="31/02" result="VS" />
-                </Col>
-              </Row>
+            <div className='top-bar-items-list'>
+              <ul>
+                <li className={active === "games" ? "top-bar-item-active" : "top-bar-list-item"} onClick={() => selectChange("games")}>Jogos</li>
+                <li className={active === "classification" ? "top-bar-item-active" : "top-bar-list-item"} onClick={() => selectChange("classification")}>Classificações</li>
+                <li className={active === "team" ? "top-bar-item-active" : "top-bar-list-item"} onClick={() => selectChange("team")}>Equipa</li>
+              </ul>
             </div>
           </div>
-          <div className="games">
-            <h3 className="games-header">Grupo B</h3>
-            <p className="games-day">Jornada 1</p>
+          {active === "games" &&
+            <div className="games">
+              <div className="games-filters">
+                <GameFilter text="Fase de Grupos - 1ª Div" />
+                <GameFilter text="Fase de Grupos - 2ª Div" />
+                <GameFilter text="Playoffs" />
+              </div>
+              <h3 className="games-header">Grupo A</h3>
+              <div className="games-list">
+                <Row className="row-game">
+                  <Col lg={4}>
+                    <Game gameDay="Jornada 1" data="31/02" result="3 - 0" />
+                  </Col>
+                  <Col lg={4}>
+                    <Game gameDay="Jornada 2" data="31/02" result="3 - 0" />
+                  </Col>
+                  <Col lg={4}>
+                    <Game gameDay="Jornada 3" data="31/02" result="VS" />
+                  </Col>
+                </Row>
+                <Row className="row-game">
+                  <Col lg={4}>
+                    <Game gameDay="Jornada 4" data="31/02" result="VS" />
+                  </Col>
+                  <Col lg={4}>
+                    <Game gameDay="Jornada 5" data="31/02" result="VS" />
+                  </Col>
+                  <Col lg={4}>
+                    <Game gameDay="Jornada 6" data="31/02" result="VS" />
+                  </Col>
+                </Row>
+              </div>
+            </div>
+          }
+          {active === "classification" &&
             <div>
-              <Row className="row-game">
-                <Col lg={4}>
-                  <Game data="31/02" result="VS" />
-                </Col>
-                <Col lg={4}>
-                  <Game data="31/02" result="VS" />
-                </Col>
-                <Col lg={4}>
-                  <Game data="31/02" result="VS" />
-                </Col>
-              </Row>
-              <Row className="row-game">
-                <Col lg={4}>
-                  <Game data="31/02" result="VS" />
-                </Col>
-                <Col lg={4}>
-                  <Game data="31/02" result="VS" />
-                </Col>
-                <Col lg={4}>
-                  <Game data="31/02" result="VS" />
-                </Col>
-              </Row>
+              <p>classification</p>
             </div>
-          </div>
+          }
+          {active === "team" &&
+            <div>
+              <p>team</p>
+            </div>
+          }
         </div>
       </div>
     </>
