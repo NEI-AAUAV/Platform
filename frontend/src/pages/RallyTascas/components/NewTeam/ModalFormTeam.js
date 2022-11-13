3fdcaa5103d384d422b7f40839a2f2d475e94e12
Dani Figueiredo <danifgrd8@gmail.com>
2022-11-13 14:31:24 +0000

    Refactor

---
+++ frontend/src/pages/RallyTascas/components/NewTeam/ModalFormTeam.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ModalFormTeam() {
+  return <div>ModalFormTeam</div>;
+}
+
+export default ModalFormTeam;
--- frontend/src/pages/RallyTascas/index.js
+++ frontend/src/pages/RallyTascas/index.js
@@ -1,15 +1,11 @@
 import { Button, Col, Input, Modal, Row, Text } from "@nextui-org/react";
 import React from "react";
 import InfoTable from "./components/InfoTable";
+import ModalTeam from "./components/NewTeam/ModalTeam";
 
 const RallyTascas = () => {
   const [visible, setVisible] = React.useState(false);
   const teamModalHandler = () => setVisible(true);
-
-  const closeHandler = () => {
-    setVisible(false);
-    console.log("closed");
-  };
 
   return (
     <Col>
@@ -37,62 +33,7 @@ const RallyTascas = () => {
         <Button shadow color="secondary" auto onClick={teamModalHandler}>
           Criar Equipa
         </Button>
-        <Modal
-          closeButton
-          aria-labelledby="modal-title"
-          //este preventClose na vista de mobile que estava a fazer continuava a fechar-se a nao ser q eu alterasse um bocadinho o tamanho (nao faz sentido)
-          preventClose
-          open={visible}
-          onClose={closeHandler}
-          css={{
-            height: "auto",
-            borderRadius: "0.5rem",
-            padding: "1rem",
-            marginLeft: "20px",
-            marginRight: "20px",
-            backgroundColor: "#010b13",
-          }}
-        >
-          <Modal.Header>
-            <Text id="modal-title" color="white" size={18}>
-              Criar uma nova
-              <Text b color="white" size={18}>
-                {" "}
-                Equipa
-              </Text>
-            </Text>
-          </Modal.Header>
-          <Modal.Body>
-            <Input
-              clearable
-              bordered
-              fullWidth
-              size="lg"
-              placeholder="Nome da Equipa"
-              status="primary"
-            />
-          </Modal.Body>
-          <Modal.Footer
-            css={{
-              display: "flex",
-              flexDirection: "row",
-              justifyContent: "center",
-            }}
-          >
-            <Button
-              auto
-              bordered
-              color="error"
-              onClick={closeHandler}
-              size="sm"
-            >
-              Fechar
-            </Button>
-            <Button auto bordered onClick={closeHandler} size="sm">
-              Criar Equipa
-            </Button>
-          </Modal.Footer>
-        </Modal>
+        <ModalTeam visible={visible} setVisible={setVisible} />
       </Row>
       <InfoTable />
     </Col>
