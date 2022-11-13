bac71d01ce31c7850f0e3752bdab2faa268eb20b
Dani Figueiredo <danifgrd8@gmail.com>
2022-11-13 15:34:49 +0000

    get data dinamically but only with static data yet

--- frontend/src/pages/RallyTascas/components/Details/DetailsModal.js
+++ frontend/src/pages/RallyTascas/components/Details/DetailsModal.js
@@ -2,16 +2,18 @@ import React from "react";
 import { Modal, Text, Button, Row } from "@nextui-org/react";
 
 function DetailsModal(props) {
+  let visible = props.visible;
+
   const closeHandler = () => {
     props.setVisible(false);
   };
+
   return (
     <Modal
+      aria-label="modalDetails"
       closeButton
       aria-labelledby="modal-title"
-      //este preventClose na vista de mobile que estava a fazer continuava a fechar-se a nao ser q eu alterasse um bocadinho o tamanho (nao faz sentido)
-      preventClose
-      open={props.visible}
+      open={visible}
       onClose={closeHandler}
       width="700px"
       css={{
@@ -40,7 +42,7 @@ function DetailsModal(props) {
           Detalhes de
           <Text b color="white" size={18}>
             {" "}
-            NOME DA EQUIPA
+            {props.selectedTeam.teamName}
           </Text>
         </Text>
       </Modal.Header>
@@ -59,7 +61,7 @@ function DetailsModal(props) {
             Nome da Equipa:
           </Text>
           <Text color="white" size={18}>
-            NOME DA EQUIPA
+            {props.selectedTeam.teamName}
           </Text>
         </Row>
         <Row
@@ -76,7 +78,7 @@ function DetailsModal(props) {
             Pontuação
           </Text>
           <Text color="white" size={18}>
-            631 Pontos
+            {props.selectedTeam.total}
           </Text>
         </Row>
         <Row>
@@ -84,142 +86,26 @@ function DetailsModal(props) {
             Checkpoints
           </Text>
         </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P1 - DETI
-          </Text>
-          <Text color="white" size={18}>
-            120 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P2 - Reitoria
-          </Text>
-          <Text color="white" size={18}>
-            100 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P3 - FITUA
-          </Text>
-          <Text color="white" size={18}>
-            120 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P4 - AFFUAV
-          </Text>
-          <Text color="white" size={18}>
-            200 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P5 - Teatro Aveirense
-          </Text>
-          <Text color="white" size={18}>
-            35 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P6 - Câmara Municipal
-          </Text>
-          <Text color="white" size={18}>
-            100 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P7 - Santos
-          </Text>
-          <Text color="white" size={18}>
-            -54 Pontos
-          </Text>
-        </Row>
-        <Row
-          css={{
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "space-between",
-            marginBottom: "1rem",
-            paddingRight: "0.5rem",
-            zIndex: 1,
-          }}
-        >
-          <Text color="white" css={{ fontWeight: "bold" }} size={18}>
-            P8 - Bar do Estudante
-          </Text>
-          <Text color="white" size={18}>
-            10 Pontos
-          </Text>
-        </Row>
+        {props.selectedTeam.scores.map((checkpoint) => (
+          <Row
+            key={checkpoint.id}
+            css={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              marginBottom: "1rem",
+              paddingRight: "0.5rem",
+              zIndex: 1,
+            }}
+          >
+            <Text color="white" css={{ fontWeight: "bold" }} size={18}>
+              P{checkpoint.id} - {checkpoint.name}
+            </Text>
+            <Text color="white" size={18}>
+              {checkpoint.score} Pontos
+            </Text>
+          </Row>
+        ))}
       </Modal.Body>
       <Modal.Footer>
         <Button auto bordered color="error" onClick={closeHandler} size="sm">
--- frontend/src/pages/RallyTascas/components/InfoTable.js
+++ frontend/src/pages/RallyTascas/components/InfoTable.js
@@ -5,67 +5,270 @@ import { EyeIcon } from "./EyeIcon";
 import { EditIcon } from "./EditIcon";
 import DetailsModal from "./Details/DetailsModal";
 
+const columns = [
+  {
+    key: "position",
+    label: "#",
+  },
+  {
+    key: "teamName",
+    label: "Equipa",
+  },
+  {
+    key: "lastCheckPoint",
+    label: "Last Checkpoint",
+  },
+  {
+    key: "total",
+    label: "Total",
+  },
+  {
+    key: "icons",
+    label: "",
+  },
+];
+
+const rows = [
+  {
+    id: 1,
+    position: "1º",
+    teamName: "Team 1",
+    lastCheckPoint: "350 Pts",
+    timeOfCheckpoint: "23:42:22",
+    total: "350 Pts",
+    scores: [
+      {
+        id: 1,
+        name: "DETI",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 2,
+        name: "Reitoria",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 3,
+        name: "DECA",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 4,
+        name: "AFFUAV",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 5,
+        name: "Teatro Aveirense",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 6,
+        name: "Câmara Municipal",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 7,
+        name: "Santos Bar",
+        score: -100,
+        time: "23:42:22",
+      },
+      {
+        id: 8,
+        name: "Bar do Estudante",
+        score: 50,
+        time: "23:42:22",
+      },
+    ],
+  },
+  {
+    id: 2,
+    position: "2º",
+    teamName: "Team 2",
+    lastCheckPoint: "250 Pts",
+    timeOfCheckpoint: "00:12:58",
+    total: "250 Pts",
+    scores: [
+      {
+        id: 1,
+        name: "DETI",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 2,
+        name: "Reitoria",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 3,
+        name: "DECA",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 4,
+        name: "AFFUAV",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 5,
+        name: "Teatro Aveirense",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 6,
+        name: "Câmara Municipal",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 7,
+        name: "Santos Bar",
+        score: -100,
+        time: "23:42:22",
+      },
+      {
+        id: 8,
+        name: "Bar do Estudante",
+        score: 50,
+        time: "23:42:22",
+      },
+    ],
+  },
+  {
+    id: 3,
+    position: "3º",
+    teamName: "Team 3",
+    lastCheckPoint: "190 Pts",
+    timeOfCheckpoint: "00:22:30",
+    total: "190 Pts",
+    scores: [
+      {
+        id: 1,
+        name: "DETI",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 2,
+        name: "Reitoria",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 3,
+        name: "DECA",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 4,
+        name: "AFFUAV",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 5,
+        name: "Teatro Aveirense",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 6,
+        name: "Câmara Municipal",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 7,
+        name: "Santos Bar",
+        score: -100,
+        time: "23:42:22",
+      },
+      {
+        id: 8,
+        name: "Bar do Estudante",
+        score: 50,
+        time: "23:42:22",
+      },
+    ],
+  },
+  {
+    id: 4,
+    position: "4º",
+    teamName: "Team 4",
+    lastCheckPoint: "175 Pts",
+    timeOfCheckpoint: "00:30:02",
+    total: "175 Pts",
+    scores: [
+      {
+        id: 1,
+        name: "DETI",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 2,
+        name: "Reitoria",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 3,
+        name: "DECA",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 4,
+        name: "AFFUAV",
+        score: 25,
+        time: "23:42:22",
+      },
+      {
+        id: 5,
+        name: "Teatro Aveirense",
+        score: 100,
+        time: "23:42:22",
+      },
+      {
+        id: 6,
+        name: "Câmara Municipal",
+        score: 50,
+        time: "23:42:22",
+      },
+      {
+        id: 7,
+        name: "Santos Bar",
+        score: -100,
+        time: "23:42:22",
+      },
+      {
+        id: 8,
+        name: "Bar do Estudante",
+        score: 50,
+        time: "23:42:22",
+      },
+    ],
+  },
+];
+
 function InfoTable() {
-  const columns = [
-    {
-      key: "position",
-      label: "#",
-    },
-    {
-      key: "teamName",
-      label: "Equipa",
-    },
-    {
-      key: "lastCheckPoint",
-      label: "Last Checkpoint",
-    },
-    {
-      key: "total",
-      label: "Total",
-    },
-    {
-      key: "icons",
-      label: "",
-    },
-  ];
-
-  const rows = [
-    {
-      id: 1,
-      position: "1º",
-      teamName: "Team 1",
-      checkpoint: "350 Pts",
-      timeOfCheckpoint: "23:42:22",
-      total: "350 Pts",
-    },
-    {
-      id: 2,
-      position: "2º",
-      teamName: "Team 2",
-      checkpoint: "250 Pts",
-      timeOfCheckpoint: "00:12:58",
-      total: "250 Pts",
-    },
-    {
-      id: 3,
-      position: "3º",
-      teamName: "Team 3",
-      checkpoint: "190 Pts",
-      timeOfCheckpoint: "00:22:30",
-      total: "190 Pts",
-    },
-    {
-      id: 4,
-      position: "4º",
-      teamName: "Team 4",
-      checkpoint: "175 Pts",
-      timeOfCheckpoint: "00:30:02",
-      total: "175 Pts",
-    },
-  ];
-
   const [visibleDetails, setVisibleDetails] = React.useState(false);
-  const detailsModalHandler = () => setVisibleDetails(true);
+  const [selectedTeam, setSelectedTeam] = React.useState(null);
+  const detailsModalHandler = () => {
+    setVisibleDetails(true);
+  };
 
   const renderCell = (team, columnKey) => {
     const cellValue = team[columnKey];
@@ -83,7 +286,13 @@ function InfoTable() {
           <Row justify="center" align="center">
             <Col css={{ d: "flex" }}>
               <Tooltip content="Details">
-                <IconButton onClick={detailsModalHandler}>
+                {/* Get selected team */}
+                <IconButton
+                  onClick={() => {
+                    setSelectedTeam(team);
+                    detailsModalHandler();
+                  }}
+                >
                   <EyeIcon size={20} fill="#979797" />
                 </IconButton>
               </Tooltip>
@@ -129,7 +338,6 @@ function InfoTable() {
         <Table.Body items={rows}>
           {(team) => (
             <Table.Row
-              //css to change the background color of the table rows
               css={{
                 color: "white",
                 fontSize: "0.875rem",
@@ -146,7 +354,14 @@ function InfoTable() {
           )}
         </Table.Body>
       </Table>
-      <DetailsModal visible={visibleDetails} setVisible={setVisibleDetails} />
+      {/* Conditional rendering */}
+      {visibleDetails && (
+        <DetailsModal
+          visible={visibleDetails}
+          setVisible={setVisibleDetails}
+          selectedTeam={selectedTeam}
+        />
+      )}
     </>
   );
 }
