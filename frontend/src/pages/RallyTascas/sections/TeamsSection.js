e463ddfd8430547e97572367b94edeb7c0da4a44
Diana Miranda <dianarrmiranda@ua.pt>
2022-11-20 17:56:31 +0000

    card

--- frontend/src/pages/RallyTascas/sections/TeamsSection.js
+++ frontend/src/pages/RallyTascas/sections/TeamsSection.js
@@ -1,8 +1,31 @@
 import InfoTeamSection from "./InfoTeamSection";
+import { Card, Col, Text } from "@nextui-org/react";
 
+export const Card3 = () => (
+    <Card css={{ bg: "$black", w: "100%" }}>
+      <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+        <Col>
+          <Text h4 color="white" >
+            Team Name
+          </Text>
+        </Col>
+      </Card.Header>    
+      <Card.Image
+      src="https://nextui.org/images/card-example-2.jpeg"
+      width="100%"
+      height={340}
+      objectFit="cover"
+      alt="Card image background"
+    />
+  </Card>
+);
 
 const TeamCard = () => {
-    return null;
+    return (
+        <>
+            <Card3 />
+        </>
+    );
 }
 
 
