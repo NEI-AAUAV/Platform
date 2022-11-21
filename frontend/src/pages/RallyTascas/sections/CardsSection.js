fe7e862f216dbad7159093a6bb6e5011d48b8029
jnluis <jnluis@ua.pt>
2022-11-21 22:14:36 +0000

    Add some styling and first onPress event

--- frontend/src/pages/RallyTascas/sections/CardsSection.js
+++ frontend/src/pages/RallyTascas/sections/CardsSection.js
@@ -1,16 +1,30 @@
 import InfoTeamSection from "./InfoTeamSection";
 import { Card, Col, Text } from "@nextui-org/react";
+import React, { useState } from "react";
 
 const Card2 = () => {
+  const Images = [
+    {
+      title: "Lemon 2",
+      img: "/images/fruit-6.jpeg",
+      text: "$8.00",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      text: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      text: "$12.20",
+    },
+  ];
+  const [visible, setVisible] = useState(false);
+
+
     return (
-        <Card css={{ bg: "$black", w: "100%" }}>
-        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-          <Col>
-            <Text h4 color="white" >
-              Team Name
-            </Text>
-          </Col>
-        </Card.Header>    
+        <Card variant="bordered" isPressable onPress={() => {setVisible(visible)}} css={{ bg: "#FC855133"  /* os últimos dois números são do canal alfa, que mostra a opacidade só do background-color*/ , w: "30%", border: "2px solid #FC8551 !important", display: visible ? "none" : "block" }}>   
         <Card.Image
         src="https://nextui.org/images/card-example-2.jpeg"
         width="100%"
@@ -18,6 +32,18 @@ const Card2 = () => {
         objectFit="cover"
         alt="Card image background"
       />
+      <Card.Footer css={{fontFamily: "Aldrich", zIndex: 1, paddingBottom: 0}}>
+          <Col>
+            <Text h3 color="#FC8551" >
+              TITLE
+            </Text>
+          </Col>
+        </Card.Footer> 
+        <Card.Body /* visible={visible} setVisible={setVisible} */ css={{fontFamily: "Aldrich", zIndex: 1, paddingTop: 0}}> 
+        <Text h4 color="#FFFFFF" >
+              Esta carta é muito louca. Confia MANO!
+            </Text>
+        </Card.Body>
     </Card>
   );
 }
