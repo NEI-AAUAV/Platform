e9ffe766e42e18b12953ec60602faaf0d3da91d7
jnluis <jnluis@ua.pt>
2022-11-21 15:20:41 +0000

    Basic Card

--- frontend/src/pages/RallyTascas/sections/CardsSection.js
+++ frontend/src/pages/RallyTascas/sections/CardsSection.js
@@ -1,12 +1,31 @@
+import InfoTeamSection from "./InfoTeamSection";
+import { Card, Col, Text } from "@nextui-org/react";
 
-const Card = () => {
-    return null;
+const Card2 = () => {
+    return (
+        <Card css={{ bg: "$black", w: "100%" }}>
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+          <Col>
+            <Text h4 color="white" >
+              Team Name
+            </Text>
+          </Col>
+        </Card.Header>    
+        <Card.Image
+        src="https://nextui.org/images/card-example-2.jpeg"
+        width="100%"
+        height={340}
+        objectFit="cover"
+        alt="Card image background"
+      />
+    </Card>
+  );
 }
 
 const CardsSection = () => {
     return (
         <>
-            <Card />
+            <Card2 />
         </>
     );
 }
