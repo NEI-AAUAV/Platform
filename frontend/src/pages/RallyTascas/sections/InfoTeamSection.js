2f26dc6b0c4e636c3a84908ce4964b7ac7552aa6
zegameiro <jose.mcgameiro@ua.pt>
2022-11-21 17:50:18 +0000

    Create card with team name

--- frontend/src/pages/RallyTascas/sections/InfoTeamSection.js
+++ frontend/src/pages/RallyTascas/sections/InfoTeamSection.js
@@ -1,6 +1,18 @@
+import { Card, Grid, Text} from "@nextui-org/react"
+import "../index.css";
 
 const InfoTeamSection = () => {
-    return null;
+    return (
+        <Grid.Container gap={2}>
+            <Grid xs={4}>
+                <Card className="rally-card-team-name">
+                    <Card.Body>
+                        <Text h3 style={{color:"white"}}>Team 1</Text>
+                    </Card.Body>
+                </Card>
+            </Grid>
+        </Grid.Container>
+    )
 }
 
 export default InfoTeamSection;
