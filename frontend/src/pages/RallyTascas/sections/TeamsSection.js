22df5fafdb97776a198da3357c2e72b93f1f2da8
Diana Miranda <dianarrmiranda@ua.pt>
2022-11-21 17:44:21 +0000

    .

--- frontend/src/pages/RallyTascas/sections/TeamsSection.js
+++ frontend/src/pages/RallyTascas/sections/TeamsSection.js
@@ -1,42 +1,67 @@
 import InfoTeamSection from "./InfoTeamSection";
 import { Card, Col, Text } from "@nextui-org/react";
+import { css, Button } from '@nextui-org/react';
 
-export const Card3 = () => (
-    <Card css={{ bg: "$black", w: "100%" }}>
-      <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-        <Col>
-          <Text h4 color="white" >
-            Team Name
-          </Text>
-        </Col>
-      </Card.Header>    
-      <Card.Image
-      src="https://nextui.org/images/card-example-2.jpeg"
-      width="100%"
-      height={340}
-      objectFit="cover"
-      alt="Card image background"
-    />
-  </Card>
-);
+
 
 const TeamCard = () => {
-    return (
-        <>
-            <Card3 />
-        </>
-    );
+  return (
+    <>
+
+      <Card css={{ w: "100%" }}>
+        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
+          <Col>
+            <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
+              Plant a tree
+            </Text>
+            <Text h4 color="white">
+              Contribute to the planet
+            </Text>
+          </Col>
+        </Card.Header>
+        <Card.Image
+          src="https://nextui.org/images/card-example-3.jpeg"
+          width="100%"
+          height={340}
+          objectFit="cover"
+          alt="Card image background"
+        />
+      </Card>
+      <Button
+        auto
+        css={{
+          borderRadius: '$xs', // radii.xs
+          border: '$space$1 solid transparent',
+          background: '$orange800', // colors.pink800
+          color: '$orange100',
+          height: '$12', // space[12]
+          boxShadow: '$md', // shadows.md
+          '&:hover': {
+            background: '$orange100',
+            color: '$orange800',
+          },
+          '&:active': {
+            background: '$orange200',
+          },
+          '&:focus': {
+            borderColor: '$orange400',
+          },
+        }}
+      >
+        Custom button
+      </Button>
+    </>
+
+  );
 }
 
 
 const TeamsSection = () => {
-    if (true)
-        return <InfoTeamSection />
-    return (
-        <>
-            <TeamCard />
-        </>
-    );
+  return (
+    <>
+      <TeamCard />
+    </>
+  );
 }
 
 export default TeamsSection;
