a113368d02481b0fb2e28b55f21a3626b1987933
Diana Miranda <dianarrmiranda@ua.pt>
2022-11-21 18:37:55 +0000

    cards

--- frontend/src/pages/RallyTascas/sections/TeamsSection.js
+++ frontend/src/pages/RallyTascas/sections/TeamsSection.js
@@ -1,56 +1,52 @@
 import InfoTeamSection from "./InfoTeamSection";
 import { Card, Col, Text } from "@nextui-org/react";
-import { css, Button } from '@nextui-org/react';
+import { Button } from '@nextui-org/react';
 
 
 
 const TeamCard = () => {
   return (
-    <>
+    <div style={{height:500}}>
 
-      <Card css={{ w: "100%" }}>
-        <Card.Header css={{ position: "absolute", zIndex: 1, top: 5 }}>
-          <Col>
-            <Text size={12} weight="bold" transform="uppercase" color="#ffffffAA">
-              Plant a tree
-            </Text>
-            <Text h4 color="white">
-              Contribute to the planet
-            </Text>
-          </Col>
-        </Card.Header>
+      <Card css={{ bg: "transparent", borderColor: "#FC8551", w: 300, margin:10 }}>
         <Card.Image
-          src="https://nextui.org/images/card-example-3.jpeg"
+          src="https://nextui.org/images/card-example-2.jpeg"
           width="100%"
-          height={340}
+          height={400}
           objectFit="cover"
           alt="Card image background"
         />
+        <Card.Footer css={{ fontFamily: "Aldrich", zIndex: 1, height:"100%" }}>
+          <Col>
+            <Text h3 color="#FC8551" >
+              TITLE
+            </Text>
+            <Button
+              auto
+              css={{
+                borderRadius: '$xs', // radii.xs
+                border: '$space$1 solid transparent',
+                background: '#FC8551', // colors.pink800
+                color: 'white',
+                height: '$12', // space[12]
+                position: 'absolute',
+                borderadius: '10px',
+                boxShadow: '$md', // shadows.md
+                '&:hover': {
+                  background: '#FC8551bb',
+                },
+              }}
+            >
+              View Team
+            </Button>
+          </Col>
+
+        </Card.Footer>
       </Card>
-      <Button
-        auto
-        css={{
-          borderRadius: '$xs', // radii.xs
-          border: '$space$1 solid transparent',
-          background: '$orange800', // colors.pink800
-          color: '$orange100',
-          height: '$12', // space[12]
-          boxShadow: '$md', // shadows.md
-          '&:hover': {
-            background: '$orange100',
-            color: '$orange800',
-          },
-          '&:active': {
-            background: '$orange200',
-          },
-          '&:focus': {
-            borderColor: '$orange400',
-          },
-        }}
-      >
-        Custom button
-      </Button>
-    </>
+
+
+
+    </div>
 
   );
 }
@@ -58,9 +54,14 @@ const TeamCard = () => {
 
 const TeamsSection = () => {
   return (
-    <>
+    <div className="d-flex flex-wrap">
+
       <TeamCard />
-    </>
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+      <TeamCard />
+    </div>
   );
 }
 
