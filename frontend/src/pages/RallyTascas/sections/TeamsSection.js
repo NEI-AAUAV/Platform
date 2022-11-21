8b7b15ddd49743634aa120851857da4d40c8bef3
Diana Miranda <dianarrmiranda@ua.pt>
2022-11-21 20:33:26 +0000

    cards

--- frontend/src/pages/RallyTascas/sections/TeamsSection.js
+++ frontend/src/pages/RallyTascas/sections/TeamsSection.js
@@ -1,25 +1,26 @@
 import InfoTeamSection from "./InfoTeamSection";
 import { Card, Col, Text } from "@nextui-org/react";
 import { Button } from '@nextui-org/react';
+import imagem from '../../Sports/img/equipa.jpg'
 
 
 
 const TeamCard = () => {
   return (
-    <div style={{height:500}}>
+    <div>
 
-      <Card css={{ bg: "transparent", borderColor: "#FC8551", w: 300, margin:10 }}>
+      <Card css={{ bg: "transparent", borderColor: "#FC8551", w: 300, marginBottom: 20 }}>
         <Card.Image
-          src="https://nextui.org/images/card-example-2.jpeg"
+          src={imagem}
           width="100%"
-          height={400}
+          height={340}
           objectFit="cover"
           alt="Card image background"
         />
-        <Card.Footer css={{ fontFamily: "Aldrich", zIndex: 1, height:"100%" }}>
+        <Card.Footer css={{ fontFamily: "Aldrich", zIndex: 1}}>
           <Col>
-            <Text h3 color="#FC8551" >
-              TITLE
+            <Text h3 color="#FFFFFF" css={{textAlign: 'center'}} >
+              Team Name
             </Text>
             <Button
               auto
@@ -27,10 +28,12 @@ const TeamCard = () => {
                 borderRadius: '$xs', // radii.xs
                 border: '$space$1 solid transparent',
                 background: '#FC8551', // colors.pink800
-                color: 'white',
-                height: '$12', // space[12]
-                position: 'absolute',
-                borderadius: '10px',
+                color: 'black',
+                fontSize:25,
+                textAlign:"center",
+                padding:5,
+                width:"100%" ,
+                borderRadius: '12px',
                 boxShadow: '$md', // shadows.md
                 '&:hover': {
                   background: '#FC8551bb',
@@ -54,8 +57,9 @@ const TeamCard = () => {
 
 const TeamsSection = () => {
   return (
-    <div className="d-flex flex-wrap">
+    <div className="d-flex flex-wrap" style={{ justifyContent: 'space-evenly'}}>
 
+      <TeamCard />
       <TeamCard />
       <TeamCard />
       <TeamCard />
