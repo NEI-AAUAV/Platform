38b5df588f14f4869e662c41c1dc31f5941a06d6
jnluis <jnluis@ua.pt>
2022-11-22 14:32:16 +0000

    Add correct OnPress function and starting to make multiple cards from one template card

--- frontend/src/pages/RallyTascas/sections/CardsSection.js
+++ frontend/src/pages/RallyTascas/sections/CardsSection.js
@@ -1,51 +1,56 @@
 import InfoTeamSection from "./InfoTeamSection";
 import { Card, Col, Text } from "@nextui-org/react";
 import React, { useState } from "react";
+import "../index.css";
 
 const Card2 = () => {
   const Images = [
     {
       title: "Lemon 2",
-      img: "/images/fruit-6.jpeg",
+      img: "./images/merch/brasao.png",
       text: "$8.00",
     },
     {
       title: "Banana",
-      img: "/images/fruit-7.jpeg",
+      img: "./images/merch/brasao.png",
       text: "$7.50",
     },
     {
       title: "Watermelon",
-      img: "/images/fruit-8.jpeg",
+      img: "./images/merch/brasao.png",
       text: "$12.20",
     },
   ];
-  const [visible, setVisible] = useState(false);
+  const [visible, setVisible] = useState(true);
 
 
     return (
-        <Card variant="bordered" isPressable onPress={() => {setVisible(visible)}} css={{ bg: "#FC855133"  /* os últimos dois números são do canal alfa, que mostra a opacidade só do background-color*/ , w: "30%", border: "2px solid #FC8551 !important", display: visible ? "none" : "block" }}>   
+      <>
+         {Images.map((item, index) => (
+        <Card key={index} variant="bordered" isPressable onPress={() => {setVisible(!visible)}} css={{ bg: "#FC855133"  /* os últimos dois números são do canal alfa, que mostra a opacidade só do background-color*/ , w: "30%",h: 450, border: "2px solid #FC8551 !important" }}>   
         <Card.Image
-        src="https://nextui.org/images/card-example-2.jpeg"
+        src={item.img}
         width="100%"
-        height={340}
+        height="100%"
         objectFit="cover"
         alt="Card image background"
       />
       <Card.Footer css={{fontFamily: "Aldrich", zIndex: 1, paddingBottom: 0}}>
           <Col>
             <Text h3 color="#FC8551" >
-              TITLE
+            {item.title}
             </Text>
           </Col>
         </Card.Footer> 
-        <Card.Body /* visible={visible} setVisible={setVisible} */ css={{fontFamily: "Aldrich", zIndex: 1, paddingTop: 0}}> 
-        <Text h4 color="#FFFFFF" >
-              Esta carta é muito louca. Confia MANO!
+        <Card.Body css={{fontFamily: "Aldrich", zIndex: 1, paddingTop: 0,  overflow: 'hidden'}}> 
+        <Text h4 color="#FFFFFF" css={{height: visible ? 0 : '200px', transition: 'height 0.2s linear', overflow: 'hidden'}}>
+        "Esta carta é muito louca. Confia MANO!"
             </Text>
         </Card.Body>
     </Card>
-  );
+    ))}
+    </>
+    );
 }
 
 const CardsSection = () => {
