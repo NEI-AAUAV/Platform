cfcb2a876a9f491b9cd9b4908e10699c3a0bbed3
zegameiro <jose.mcgameiro@ua.pt>
2022-11-22 15:41:13 +0000

    Added cards with team members team name and checkpoint

--- frontend/src/pages/RallyTascas/sections/InfoTeamSection.js
+++ frontend/src/pages/RallyTascas/sections/InfoTeamSection.js
@@ -7,7 +7,30 @@ const InfoTeamSection = () => {
             <Grid xs={4}>
                 <Card className="rally-card-team-name">
                     <Card.Body>
-                        <Text h3 style={{color:"white"}}>Team 1</Text>
+                        <Text style={{color:"#FC8551"}}>9th place</Text>
+                        <Text h3 style={{color:"white"}}>Team Name</Text>
+                    </Card.Body>
+                </Card>
+            </Grid>
+            <Grid xs={4}>
+                <Card className="rally-card-team-information">
+                    <Card.Body>
+                        <Text h3 style={{color:"#FC8551",fontWeight:"bold"}}>{'>'} TEAM MEMBERS</Text>
+                        <Text style={{color:"white"}}>Team Member 1</Text>
+                        <Text style={{color:"white"}}>Team Member 2</Text>
+                        <Text style={{color:"white"}}>Team Member 3</Text>
+                        <Text style={{color:"white"}}>Team Member 4</Text>
+                        <Text style={{color:"white"}}>Team Member 5</Text>
+                    </Card.Body>
+                </Card>
+            </Grid>
+            <Grid xs={4}>
+                <Card className="rally-card-team-checkpoint">
+                    <Card.Body>
+                        <Text h3 style={{color:"#FC8551",fontWeight:"bold"}}>{'>'} CHECKPOINT</Text>
+                        <Text style={{color:"white"}}>Last Checkpoint</Text>
+                        <Text style={{color:"white"}}>Score: <span style={{color:"red"}}>10ptx</span></Text>
+                        <Text style={{color:"white"}}>Date: --/--/-- --:--</Text>
                     </Card.Body>
                 </Card>
             </Grid>
