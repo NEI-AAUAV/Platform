d789803372b0230b0c0ccca571b0e875581e711a
Bárbara Galiza <barnobregali02@gmail.com>
2022-11-24 00:25:26 +0000

    added autoincrement

--- rallytascas-api/app/models/user.py
+++ rallytascas-api/app/models/user.py
@@ -5,7 +5,7 @@ from app.db.base_class import Base
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String)
     email = Column(String)
     is_staff = Column(Boolean)
