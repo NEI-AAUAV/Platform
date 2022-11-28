ccecfdfc4843aa7a63944cf7f3ea5a930816e04f
jnluis <jnluis@ua.pt>
2022-11-28 15:57:15 +0000

    Cards appearing now when is supossed to dummy data

--- frontend/src/pages/RallyTascas/sections/CardsSection.js
+++ frontend/src/pages/RallyTascas/sections/CardsSection.js
@@ -2,19 +2,21 @@ import { Card, Col, Text } from "@nextui-org/react";
 import React, { useEffect,useState } from "react";
 import "../index.css";
 import service from 'services/RallyTascasService';
+import { useRallyAuth } from "stores/useRallyAuth";
 
-const Card2 = ({item, index}) => {
+const {isStaff, isAdmin } = useRallyAuth(state => state);
+
+const Card2 = ({item, index,disabled}) => {
   
   const [textvisible, settextVisible] = useState(true);
 
     return (
       <>      
-        <Card key={index} variant="bordered" isPressable onPress={() => {settextVisible(!textvisible)}} css={{ bg: "#FC855133"  /* os últimos dois números são do canal alfa, que mostra a opacidade só do background-color*/ , w: "280px",h: 400, border: "2px solid #FC8551 !important", margin: 15 }}>   
+        <Card key={index} variant="bordered" isPressable onPress={() => {settextVisible(!textvisible)}} css={{ bg: "#FC855133"  /* os últimos dois números são do canal alfa, que mostra a opacidade só do background-color*/ , w: "280px",h: 400, border: "2px solid #FC8551 !important", margin: 15,opacity:disabled ? 0.5:1 }}>   
         <Card.Image
         src={item.img}
-        width="100%"
-        height="100%"
-        objectFit="cover"
+        css={{w:"3000px",objectFit: "fill",autoResize: true }}
+        // falta colocar a imagem a dar resize para a full Width
         alt="Card image background"
       />
       <Card.Footer css={{fontFamily: "Akshar", zIndex: 1, paddingBottom: 0}}>
@@ -25,7 +27,7 @@ const Card2 = ({item, index}) => {
           </Col>
         </Card.Footer> 
         <Card.Body css={{fontFamily: "Akshar", zIndex: 1, paddingTop: 0,  overflow: 'hidden'}}> 
-        <Text h4 color="#FFFFFF" css={{height: textvisible ? 0 : '200px', transition: 'height 0.2s linear', overflow: 'hidden'}}>
+        <Text h4 color="#FFFFFF" css={{height: textvisible ? 0 : '175px', transition: 'height 0.2s linear', overflow: 'hidden'}}>
         {item.text}
             </Text>
         </Card.Body>
@@ -37,59 +39,61 @@ const Card2 = ({item, index}) => {
 const CardsSection = () => {
   const Images = [
     {
-      title: "Lemon 2",
-      img: "./images/merch/brasao.png",
-      text: "Carta BACANA",
+      title: "Pergunta-Pass❔",
+      img: "./images/CardsSection/CABULA CABULOSA.svg",
+      text: "Poupa alguns neurónios! Tens direito a passar uma pergunta à frente, no entanto, recebes 6 pontos, em vez de 8",
     },
     {
-      title: "Banana",
-      img: "./images/merch/brasao.png",
-      text: "Louca Carta",
+      title: "Desafio-pass🎯",
+      img: "./images/CardsSection/SKIP GYM DAY.svg",
+      text: "Tens direito a passar um desafio à frente, no entanto, recebes 8 pontos em vez de 10",
     },
     {
-      title: "Watermelon",
-      img: "./images/merch/brasao.png",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et nulla orci. Donec nec felis semper, cursus risus quis, fringilla nisi. Nulla imperdiet nibh a mi posuere lacinia in ut.",
+      title: "Grego-Pass🤮",
+      img: "./images/CardsSection/CHAMAR O GREGORIO.svg",
+      text: "Deita tudo cá para fora! Um dos elementos tem direito a vomitar uma vez.",
     },
   ];
-  const [Team, setTeam] = useState({  "name": "A miha equipa bonita",
-  "id": 0,
-  "scores": [
-    0
-  ],
-  "times": [
-    "2022-11-27T16:16:03.903Z"
-  ],
-  "classification": 1,
-  "members": [
-    {
-      "team_id": 0,
-      "name": "Leandro Silva",
-      "id": 0 
-    }
-  ],
-  "card1": true,
-  "card2": false,
-  "card3": true});
+  const [Team, setTeam] = useState({
+    "name": "A miha equipa bonita",
+    "id": 0,
+    "scores": [
+      0
+    ],
+    "times": [
+      "2022-11-27T16:16:03.903Z"
+    ],
+    "classification": 1,
+    "members": [
+      {
+        "team_id": 0,
+        "name": "Leandro Silva",
+        "id": 0
+      }
+    ],
+    "card1": -1,
+    "card2": 0,
+    "card3": 1
+  });
       // Get API data when component renders
+       /*
       useEffect(() => {
         service.getOwnTeam()
             .then((data) => {
                 setTeam(data);
             });
-    }, []);
+    }, []);  */
  
     return (
         <div className="d-flex flex-wrap" style={{ justifyContent:"space-evenly"}}>
          {Images.map((item, index) => (
-            Team[`card${index}`] && <Card2 item={item} index={index} />
+            Team?.[`card${index+1}`] !== -1 && <Card2 item={item} index={index} disabled={Team?.[`card${index+1}`] !== 0} />
     ))}
-      {(!(Team.card1 || Team.card2 || Team.card3) ?
+      {((Team.card1 ==-1 && Team.card2 ==-1  && Team.card3 ==-1 ) ?
     <text style={{ fontFamily:"Akshar",color:"white", fontSize:"60px",textAlign:"center",marginTop:"3rem",fontWeight: "bold"}}><p style={{all: "inherit"}}>Não tens nenhuma carta. Vai beber!</p></text>
     :
     null
     )}
-    
       </div>
     );
 }
