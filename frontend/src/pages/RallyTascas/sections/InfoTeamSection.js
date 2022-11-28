21c6ef6b44dcbe015e92584db4289857df1a4584
zegameiro <jose.mcgameiro@ua.pt>
2022-11-28 01:05:13 +0000

    Rato voador

--- frontend/src/pages/RallyTascas/sections/InfoTeamSection.js
+++ frontend/src/pages/RallyTascas/sections/InfoTeamSection.js
@@ -1,4 +1,5 @@
 import { Card,Text} from "@nextui-org/react"
+import LeaderBoard from "../components/LeaderBoard";
 import "../index.css";
 
 const InfoTeamSection = () => {
@@ -15,7 +16,7 @@ const InfoTeamSection = () => {
     return (
         <>
             {data.map((item, index) => (
-                <div key={index} className="d-flex flex-wrap" style={{ justifyContent:"space-evenly",fontFamily:"Aldrich"}}>
+                <div key={index} className="d-flex flex-wrap" style={{ justifyContent:"space-evenly",fontFamily:"Aldrich",width:"100%",justifyContent:"space-around"}}>
                     <div className="rally-tascas-column">
                         <div className="rally-tascas-row">
                             <Card variant="bordered" className="rally-card-team-image">   
@@ -60,7 +61,7 @@ const InfoTeamSection = () => {
                                 <Card.Body>
                                     <Text h3 style={{color:"#FC8551",fontWeight:"bold"}}>{'>'} CHECKPOINT</Text>
                                     <Text style={{color:"white"}}>Last Checkpoint</Text>
-                                    <Text style={{color:"white"}}>Score: <span style={{color:"red"}}>{item.checkpoint_points} PTS</span></Text>
+                                    <Text style={{color:"white"}}>Score: <span style={{color:"rgb(255, 70, 70)"}}>{item.checkpoint_points} PTS</span></Text>
                                     <Text style={{color:"white"}}>Date: {item.checkpoint_time}</Text>
                                 </Card.Body>
                             </Card>
@@ -69,10 +70,13 @@ const InfoTeamSection = () => {
                             <Card variant="bordered" className="rally-card-team-points">
                                 <Card.Body>
                                     <Text h3 style={{color:"#FC8551",fontWeight:"bold"}}>{'>'} TOTAL SCORE</Text>
-                                    <Text style={{color:"red"}}>{item.total_score} PTS</Text>
+                                    <Text style={{color:"rgb(255, 70, 70)"}}>{item.total_score} PTS</Text>
                                 </Card.Body>
                             </Card>
                         </div>
+                    </div>
+                    <div className="rally-tascas-column">
+                        <LeaderBoard/>
                     </div>
                 </div>
             ))}
