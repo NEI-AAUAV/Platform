4a134fdf1960212befaf327830ff3841dafc4b86
leand12 <leandrosilva12@ua.pt>
2022-12-01 04:38:47 +0000

    deployment fixed

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -41,7 +41,7 @@ const routes = [
     path: "/",
     element: <SimpleLayout />,
     children: [
-      { path: "/breakthebars", element: <RallyTascas /> },
+      !config.PRODUCTION && { path: "/breakthebars", element: <RallyTascas /> },
     ],
   },
   {
