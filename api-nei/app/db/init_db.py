0f14d521ce2508a58fdf869afd6f3d79d3668969
leand12 <leandrosilva12@ua.pt>
2023-02-27 17:58:50 +0000

    tape fix on migrations

--- api-nei/app/db/init_db.py
+++ api-nei/app/db/init_db.py
@@ -1,11 +1,10 @@
 from sqlalchemy.schema import CreateSchema, DropSchema
+from sqlalchemy.sql import text
 from sqlalchemy import event
-from datetime import date, datetime
 
 from app.core.config import settings
 from .base import Base
 from .session import engine
-
         
 # make sure all SQL Alchemy models are imported (app.db.base) before initializing DB
 # otherwise, SQL Alchemy might fail to initialize relationships properly
@@ -17,9 +16,15 @@ def init_db() -> None:
     # But if you don't want to use migrations, create
     # the tables with Base.metadata.create_all(bind=engine)
 
-    if not engine.dialect.has_schema(engine, schema=settings.SCHEMA_NAME):
-        event.listen(Base.metadata, "before_create", CreateSchema(settings.SCHEMA_NAME), insert=True)
+    with engine.connect() as conn:
+        if not engine.dialect.has_schema(conn, schema=settings.SCHEMA_NAME):
+            event.listen(Base.metadata, "before_create", CreateSchema(settings.SCHEMA_NAME), insert=True)
 
-    Base.metadata.reflect(bind=engine, schema=settings.SCHEMA_NAME)
-    Base.metadata.create_all(bind=engine, checkfirst=True)
+            Base.metadata.reflect(bind=engine, schema=settings.SCHEMA_NAME)
+            Base.metadata.create_all(bind=engine, checkfirst=True)
 
+            # Populate Database
+            file = open("./app/db/backup-0001.sql")
+            stmts = text(file.read())
+            conn.execute(stmts)
+            conn.commit()
