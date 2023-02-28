21af8aca41ed195020c2264c235d3252bc3717b0
Blosuhm <zakhar.kruptsala@gmail.com>
2023-02-28 12:25:09 +0000

    card image css reset

--- web-nei/src/components/CardMerch/index.js
+++ web-nei/src/components/CardMerch/index.js
@@ -2,7 +2,10 @@ const CardMerch = (props) => {
   return (
     <>
       <div className="flex flex-col rounded justify-center items-center bg-primary p-3 shadow hover:scale-105 transition ease-in duration-100">
-        <img src={props.img} className="max-h-48" />
+        <img
+          src={props.img}
+          className="max-w-full block object-cover object-center"
+        />
         <h2>{props.title}</h2>
         <div className="flex flex-row justify-center items-center">
           <h3 className="text-2xl font-bold">{props.price}</h3>
