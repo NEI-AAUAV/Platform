815c78249679442655b69b7e648425b8dd72923a
Blosuhm <zakhar.kruptsala@gmail.com>
2023-03-03 21:09:38 +0000

    added a className prop and gave the image a max width and max height

--- web-nei/src/components/CardMerch/index.js
+++ web-nei/src/components/CardMerch/index.js
@@ -1,23 +1,26 @@
-const CardMerch = ({ img, title, price }) => {
-  const priceFormatted = price.toLocaleString("pt-PT", {
+const CardMerch = ({ img, title, price, className }) => {
+  // price should be a number
+  const formattedPrice = price.toLocaleString("pt-PT", {
     style: "currency",
     currency: "EUR",
   });
 
   return (
     <>
-      <div className="flex flex-col rounded justify-center items-center bg-base-200 p-3 shadow hover:scale-105 transition ease-in duration-100">
+      <div
+        className={`flex flex-col w-min rounded justify-center items-center bg-base-200 p-3 shadow hover:scale-105 transition ease-in duration-100 ${className}`}
+      >
         <img
           src={img}
-          className="max-w-full block object-cover object-center "
+          className="max-w-sm max-h-96 block object-cover object-center"
         />
         <h2>{title}</h2>
         <div className="flex flex-row justify-center items-center">
-          <h3 className="text-2xl font-bold">{priceFormatted}</h3>
+          <h3 className="text-2xl font-bold">{formattedPrice}</h3>
         </div>
-        {/* I dont know if the button is going to stay but ill keep it for now */}
+        {/* I don't know if the button is going to stay but i'll keep it for now */}
         <div className="flex flex-row justify-center items-center">
-          <a href="#">
+          <a>
             <button className="btn btn-outline btn-secondary font-bold py-2 px-4 rounded">
               Comprar
             </button>
