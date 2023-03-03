817cf87f327ce877cad51a582b98da234d36eba3
Blosuhm <zakhar.kruptsala@gmail.com>
2023-03-03 20:28:11 +0000

    some small adjustments for the price of the card

--- web-nei/src/components/CardMerch/index.js
+++ web-nei/src/components/CardMerch/index.js
@@ -1,15 +1,21 @@
-const CardMerch = (props) => {
+const CardMerch = ({ img, title, price }) => {
+  const priceFormatted = price.toLocaleString("pt-PT", {
+    style: "currency",
+    currency: "EUR",
+  });
+
   return (
     <>
-      <div className="flex flex-col rounded justify-center items-center bg-primary p-3 shadow hover:scale-105 transition ease-in duration-100">
+      <div className="flex flex-col rounded justify-center items-center bg-base-200 p-3 shadow hover:scale-105 transition ease-in duration-100">
         <img
-          src={props.img}
-          className="max-w-full block object-cover object-center"
+          src={img}
+          className="max-w-full block object-cover object-center "
         />
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="flex flex-row justify-center items-center">
-          <h3 className="text-2xl font-bold">{props.price}</h3>
+          <h3 className="text-2xl font-bold">{priceFormatted}</h3>
         </div>
+        {/* I dont know if the button is going to stay but ill keep it for now */}
         <div className="flex flex-row justify-center items-center">
           <a href="#">
             <button className="btn btn-outline btn-secondary font-bold py-2 px-4 rounded">
