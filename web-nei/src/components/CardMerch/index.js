8c277cb677d084c620a8e99e7f26cd3a1307c1af
Blosuhm <zakhar.kruptsala@gmail.com>
2023-03-04 00:49:15 +0000

    update head, some tweaks in the card component and some demos in the merchandising page and components page

--- web-nei/src/components/CardMerch/index.js
+++ web-nei/src/components/CardMerch/index.js
@@ -1,3 +1,17 @@
+/**
+ * Card component for merchandising
+ *
+ * @param {string} img - Image URL
+ * @param {string} title - Merch title
+ * @param {number} price - Merch price
+ * @param {string} className - Additional classes to be added to the component
+ *
+ * @returns {JSX.Element}
+ *
+ * @example
+ * <CardMerch img="https://i.imgur.com/3ZQ3Z9C.png" title="Merch Title" price={10} />
+ */
+
 const CardMerch = ({ img, title, price, className }) => {
   // price should be a number
   const formattedPrice = price.toLocaleString("pt-PT", {
@@ -8,23 +22,18 @@ const CardMerch = ({ img, title, price, className }) => {
   return (
     <>
       <div
-        className={`flex flex-col w-min rounded justify-center items-center bg-base-200 p-3 shadow hover:scale-105 transition ease-in duration-100 ${className}`}
+        className={`flex flex-col max-w-md w-fit rounded justify-center items-center bg-base-200 p-6 shadow-md my-2 ${className}`}
       >
         <img
           src={img}
-          className="max-w-sm max-h-96 block object-cover object-center"
+          className="max-w-sm max-h-96 block object-cover object-center hover:scale-110 transition ease-in duration-150 p-6 "
+          alt="Imagem Disponível em Breve"
         />
-        <h2>{title}</h2>
-        <div className="flex flex-row justify-center items-center">
-          <h3 className="text-2xl font-bold">{formattedPrice}</h3>
-        </div>
-        {/* I don't know if the button is going to stay but i'll keep it for now */}
-        <div className="flex flex-row justify-center items-center">
-          <a>
-            <button className="btn btn-outline btn-secondary font-bold py-2 px-4 rounded">
-              Comprar
-            </button>
-          </a>
+        <div className="card-body justify-end">
+          <h1>{title}</h1>
+          <div className="flex justify-center items-center">
+            <h3 className="text-lg font-medium">Preço: {formattedPrice}</h3>
+          </div>
         </div>
       </div>
     </>
