3d13f00cce8591b975e481c5c7ff94de028f7e62
João Capucho <jcapucho7@gmail.com>
2023-03-12 20:01:13 +0000

    api-nei: Split auth into submodules

---
+++ api-nei/app/api/api_v1/auth/_deps.py
@@ -0,0 +1,220 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer, SecurityScopes
+from fastapi.responses import JSONResponse, Response
+
+from sqlalchemy.orm import Session
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from jose import JWTError, jwt
+from datetime import datetime
+from typing import Any
+from email_validator import caching_resolver
+
+from app.models.user import User
+from app.models.device_login import DeviceLogin
+from app.schemas.user import ScopeEnum
+from app.core.config import settings
+
+
+with open(settings.JWT_SECRET_KEY_PATH, "r") as file:
+    private_key = file.read()
+
+with open(settings.JWT_PUBLIC_KEY_PATH, "r") as file:
+    public_key = file.read()
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=settings.API_V1_STR + "/auth/login",
+    scopes={
+        ScopeEnum.ADMIN: "Full access to everything.",
+        ScopeEnum.MANAGER_FAMILY: "Edit faina family.",
+        ScopeEnum.MANAGER_TACAUA: "Edit data related to tacaua.",
+        ScopeEnum.MANAGER_NEI: "Edit data related to nei.",
+    },
+)
+
+
+pwd_context = CryptContext(
+    # Algoritmos aceites para a verificação das password hashed.
+    #
+    # O primeiro algoritmo da lista será usado para calcular a hash das passwords novas.
+    #
+    # NOTA: Eu não sou perito da coisa mas esta era a recomendação mais alta ¯\_(ツ)_/¯
+    # https://passlib.readthedocs.io/en/stable/narr/quickstart.html#recommended-hashes
+    # https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html
+    schemes=["argon2"],
+    # Esta opção marca todas as hash de passwords que usam algoritmos que não o primeiro
+    # no `schemes` como precisando de migração para o novo algoritmo
+    deprecated="auto",
+    # Alguns algoritmos têm um tamanho limite nas passwords e truncam as passwords
+    # maiores que esse limite. Isso é bastante mau de um ponto de vista de segurança
+    # então deve retornar-se um erro em vez de as truncar.
+    #
+    # https://cheatsheetseries.owasp.org/cheatsheets/Password_Storage_Cheat_Sheet.html#input-limits
+    # https://cheatsheetseries.owasp.org/cheatsheets/Authentication_Cheat_Sheet.html#implement-proper-password-strength-controls
+    truncate_error=True,
+)
+
+
+# Create the dns resolver to use for email validation (Supports caching).
+resolver = caching_resolver(timeout=15)
+
+
+class Token(BaseModel):
+    """Authentication data"""
+
+    access_token: str
+    token_type: str
+
+
+def hash_password(password: str) -> str:
+    """Calculates the password hash
+
+    **Parameters**
+    * `password`: The plaintext password
+
+    **Returns**
+    The hashed password as a string
+    """
+    return pwd_context.hash(password)
+
+
+def create_token(payload: dict[str, Any]) -> str:
+    """Creates a new token
+
+    **Parameters**
+    * `payload` the token data payload
+
+    **Returns**
+    A JWT encoded token with the provided payload
+    """
+    encoded_jwt = jwt.encode(
+        payload,
+        private_key,
+        algorithm=settings.JWT_ALGORITHM,
+    )
+    return encoded_jwt
+
+
+def decode_token(token: str) -> dict[str, Any]:
+    return jwt.decode(
+        token,
+        public_key,
+        algorithms=[settings.JWT_ALGORITHM],
+        options={
+            "require_iat": True,
+            "require_exp": True,
+            "require_sub": True,
+        },
+    )
+
+
+async def verify_token(
+    security_scopes: SecurityScopes,
+    token: str = Depends(oauth2_scheme),
+) -> dict[str, Any]:
+    """Dependency for user authentication"""
+    if security_scopes.scopes:
+        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+    else:
+        authenticate_value = "Bearer"
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": authenticate_value},
+    )
+    try:
+        payload = decode_token(token)
+        token_scopes = payload.get("scopes", [])
+    except JWTError:
+        raise credentials_exception
+
+    # Bypass scopes for admin
+    if ScopeEnum.ADMIN in token_scopes:
+        return payload
+
+    # Verify that the token has all the necessary scopes
+    for scope in security_scopes.scopes:
+        if scope not in token_scopes:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not enough permissions",
+                headers={"WWW-Authenticate": authenticate_value},
+            )
+
+    return payload
+
+
+def generate_response(
+    db: Session,
+    user: User,
+    device_login: DeviceLogin | None = None,
+) -> Response:
+    """Creates a new authorization response for a user
+
+    **Parameters**
+    * `db`: A SQLAlchemy ORM session
+    * `user`: The user's information
+    * `device_login`: The device login information if refreshing the session, otherwise None
+
+    **Returns**
+    A response with the refresh token cookie set and the
+    access token in the body
+    """
+
+    # Measure once the current time, the same value must be passed to the
+    # created device login and refreshed token otherwise the token could be
+    # denied because the dates mismatch.
+    iat = datetime.utcnow()
+
+    if device_login is None:
+        # Create a new session and add it to the database if no session exists
+        device_login = DeviceLogin(
+            user_id=user.id,
+            session_id=int(iat.timestamp()),
+            refreshed_at=iat,
+            expires_at=iat + settings.REFRESH_TOKEN_EXPIRE,
+        )
+        db.add(device_login)
+    else:
+        # Update the last time the token was refreshed if the session already exists
+        device_login.refreshed_at = iat
+
+    # Flush all changes to the database to ensure consistency
+    db.commit()
+
+    access_token = create_token(
+        {
+            "iat": iat,
+            "exp": iat + settings.ACCESS_TOKEN_EXPIRE,
+            # JWT requires 'sub' to be a string
+            "sub": str(user.id),
+            "nmec": user.nmec,
+            "name": user.name,
+            "surname": user.surname,
+            "scopes": user.scopes,
+        }
+    )
+    refresh_token = create_token(
+        {
+            "iat": iat,
+            "exp": device_login.expires_at,
+            # JWT requires 'sub' and 'sid' to be a string
+            "sub": str(user.id),
+            "sid": device_login.session_id,
+        },
+    )
+
+    token_data = Token(access_token=access_token, token_type="bearer")
+
+    response = JSONResponse(content=token_data.dict())
+    response.set_cookie(
+        key="refresh",
+        value=refresh_token,
+        expires=device_login.expires_at,
+        secure=settings.PRODUCTION,
+        httponly=True,
+        samesite="strict",
+        # Only pass the cookie to the refresh endpoint
+        path=settings.API_V1_STR + "/auth/refresh",
+    )
+    return response
---
+++ api-nei/app/api/api_v1/auth/session.py
@@ -0,0 +1,101 @@
+from fastapi import APIRouter, Depends, HTTPException, Cookie, status
+
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from jose import JWTError
+from datetime import datetime
+from typing import Literal
+
+from app import crud
+from app.api import deps
+from app.models.device_login import DeviceLogin
+
+from ._deps import Token, generate_response, decode_token
+
+router = APIRouter()
+
+
+@router.post(
+    "/refresh",
+    responses={401: {"description": "Invalid refresh token"}},
+    response_model=Token,
+)
+async def refresh(
+    db: Session = Depends(deps.get_db), refresh: str | None = Cookie(default=None)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Refresh"},
+    )
+
+    if refresh is None:
+        raise credentials_exception
+
+    try:
+        payload = decode_token(refresh)
+        # Extract all needed fields inside a `try` in case a token
+        # has a bad payload.
+        user_id = int(payload["sub"])
+        session_id = int(payload["sid"])
+        issued_at = payload["iat"]
+    except (JWTError, ValueError, KeyError):
+        raise credentials_exception
+
+    # Get the token's session from the database
+    device_login = db.query(DeviceLogin).get((user_id, session_id))
+    if device_login is None:
+        raise credentials_exception
+
+    # Safety check that the session hasn't expired, the token should already
+    # encode this.
+    if device_login.expires_at < datetime.now():
+        raise credentials_exception
+
+    # Check that this token issue date isn't before the last token refresh, if
+    # this happens it might mean someone got the token and is trying to replay it
+    if int(device_login.refreshed_at.timestamp()) > issued_at:
+        raise credentials_exception
+
+    user = crud.user.get(db, user_id)
+    if user is None:
+        raise credentials_exception
+
+    return generate_response(db, user, device_login)
+
+
+class VerifyResponse(BaseModel):
+    status: Literal["success"]
+    message: str
+
+
+@router.get(
+    "/verify",
+    responses={401: {"description": "Invalid confirmation token"}},
+    response_model=VerifyResponse,
+)
+async def verify(
+    token: str,
+    db: Session = Depends(deps.get_db),
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid confirmation token",
+        headers={"WWW-Authenticate": "Verify"},
+    )
+
+    try:
+        payload = decode_token(token)
+        # Extract all needed fields inside a `try` in case a token
+        # has a bad payload.
+        user_id = int(payload["sub"])
+    except (JWTError, ValueError, KeyError):
+        raise credentials_exception
+
+    user = crud.user.get(db, user_id)
+    if user is None:
+        raise credentials_exception
+
+    crud.user.update(db, db_obj=user, obj_in={"active": True})
+
+    return VerifyResponse(status="success", message="Account verified successfully")
