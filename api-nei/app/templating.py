0bc059411ee11c7bbb9f2d6ddf8eae6e52ed3d47
João Capucho <jcapucho7@gmail.com>
2023-03-14 21:28:57 +0000

    api-nei: Implement password resets

--- api-nei/app/templating.py
+++ api-nei/app/templating.py
@@ -30,3 +30,49 @@ def render_email_registration_templates(
         _emailRegistrationTemplateHtml.render(renderData),
         _emailRegistrationTemplateText.render(renderData),
     )
+
+
+_passwordResetTemplateHtml = _env.get_template("PasswordReset.html")
+_passwordResetTemplateText = _env.get_template("PasswordReset.txt")
+
+
+def render_password_reset_templates(
+    email: str, name: str, token: str
+) -> tuple[str, str]:
+    """Renders the email templates for password resets
+
+    **Parameters**
+    * `email`: The email of the recipient
+    * `name`: The name of the user
+    * `token`: The password reset token
+
+    **Returns**
+    A tuple with the rendered html email and the text email
+    """
+    endpoint = settings.HOST + settings.PASSWORD_RESET_ENDPOINT
+    renderData = {"email": email, "name": name, "token": token, "endpoint": endpoint}
+    return (
+        _passwordResetTemplateHtml.render(renderData),
+        _passwordResetTemplateText.render(renderData),
+    )
+
+
+_passwordChangedTemplateHtml = _env.get_template("PasswordChanged.html")
+_passwordChangedTemplateText = _env.get_template("PasswordChanged.txt")
+
+
+def render_password_changed_templates(email: str, name: str) -> tuple[str, str]:
+    """Renders the email templates for password changes notifications
+
+    **Parameters**
+    * `email`: The email of the recipient
+    * `name`: The name of the user
+
+    **Returns**
+    A tuple with the rendered html email and the text email
+    """
+    renderData = {"email": email, "name": name}
+    return (
+        _passwordChangedTemplateHtml.render(renderData),
+        _passwordChangedTemplateText.render(renderData),
+    )
