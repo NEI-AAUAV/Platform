e816963e6a523c00898d55f90b4c9d076e9a86af
Tomas Victal <tomasvictal@gmai.com>
2023-03-25 23:06:06 +0000

    created services

--- web-nei/src/services/TacaUAService.js
+++ web-nei/src/services/TacaUAService.js
@@ -46,7 +46,102 @@ client.interceptors.response.use(function (response) {
 
 
 class TacaUAService {
+    //----Modalidade---- IMAGE??
+    async getModalitys(){
+        return await client.get('/modality/');
+    }
+    async createModality(params,image){
+        let data = new FormData();
+        data.append('image',image,image.name);
+        data.append('modality',{params})
+        return await client.post('/modality',data);
+    }
+    async getModalitybyId(id){
+        return await client.get(`/modality/${id}`);
+    }
+    async updateModality(id,params,image){
+        let data = new FormData();
+        data.append('image',image,image.name);
+        data.append('modality',{params})
+        return await client.put(`/modality/${id}`,data);
+    }
+    async removeModality(id){
+        return await client.delete(`/modality/${id}`);
+    }
 
+    //----Competiçao----
+    async createCompetition(params){
+        return await client.post('/competition/',{params});
+    }
+    async updateCompetition(id,params){
+        return await client.put(`/competition/${id}`,{params});
+    }
+    async removeCompetition(id){
+        return await client.delete(`/competition/${id}`);
+    }
+
+    //----Equipa---- IMAGE??
+    async createTeam(params,image){
+        let data = new FormData();
+        data.append('image',image,image.name);
+        data.append('team',{params})
+        return await client.post('/team/',data);
+    }
+    async updateTeam(id,params,image){
+        let data = new FormData();
+        data.append('image',image,image.name);
+        data.append('team',{params})
+        return await client.put(`/team/${id}`,data)
+    }
+    async removeTeam(id){
+        return await client.delete(`/team/${id}`);
+    }
+
+    //----Participante----
+    async createParticipant(params){
+        return await client.post('/participant/',{params});
+    }
+    async updateParticipant(id,params){
+        return await client.put(`/participant/${id}`,{params})
+    }
+    async removeParticipant(id){
+        return await client.delete(`/participant/${id}`);
+    }
+
+
+    //----Curso----
+    async getCourses(){
+        return await client.get('/course/');
+    }
+    async createCourse(params){
+        return await client.post('/course',{params});
+    }
+    async getCoursebyId(id){
+        return await client.get(`/course/${id}`);
+    }
+    async updateCourse(id,params){
+        return await client.put(`/course/${id}`,{params});
+    }
+    async removeCourse(id){
+        return await client.delete(`/course/${id}`);
+    }
+
+
+    //----Grupo----
+    async createGroup(params){
+        return await client.post('/group/',{params});
+    }
+    async updateGroup(id,params){
+        return await client.put(`/group/${id}`,{params})
+    }
+    async removeGroup(id){
+        return await client.delete(`/group/${id}`);
+    }
+
+    //----Match----
+    async updateMatch(id,params){
+        return await client.put(`/match/${id}`,{params})
+    }
 }
 
 export default new TacaUAService();
