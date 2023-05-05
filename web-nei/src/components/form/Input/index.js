10656e614e940ef387703370dd6cf5b58eca441d
Blosuhm <zakhar.kruptsala@gmail.com>
2023-05-06 00:35:35 +0100

    Changed form code and fully added feedback on register

--- web-nei/src/components/form/Input/index.js
+++ web-nei/src/components/form/Input/index.js
@@ -1,35 +1,33 @@
 import { forwardRef } from "react";
 import classNames from "classnames";
 
-const Input = forwardRef(({ id, label, error, icon, ...props }, ref) => (
-  <>
-    {!!label && (
-      <label htmlFor={id} className="label">
-        <span className="label-text">{label}</span>
-      </label>
-    )}
-    <div className="relative">
-      <input
-        ref={ref}
-        id={id}
-        type="text"
-        className={classNames(
-          "input-bordered input w-full",
-          icon && "pr-12",
-          error && "!input-error"
-        )}
-        {...props}
-      />
-      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
-        {icon}
+const Input = forwardRef(
+  ({ id, label, error, icon, className, ...props }, ref) => (
+    <div className={className}>
+      {!!label && (
+        <label htmlFor={id} className="label">
+          <span className="label-text">{label}</span>
+        </label>
+      )}
+      <div className="relative">
+        <input
+          ref={ref}
+          id={id}
+          type="text"
+          className={classNames(
+            "input-bordered input w-full",
+            icon && "pr-12",
+            error && "!input-error"
+          )}
+          {...props}
+        />
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-4">
+          {icon}
+        </div>
       </div>
+      {!!error && <p className="message-error">{error?.message}</p>}
     </div>
-    {!!error && (
-      <p className="message-error">
-        {error?.message}
-      </p>
-    )}
-  </>
-));
+  )
+);
 
 export default Input;
