ec3722d94687e7f7fb54a49149a089beee309799
Tomas Victal <tomasvictal@gmai.com>
2023-05-12 22:37:26 +0100

    card done

--- web-nei/src/components/GameCard/index.js
+++ web-nei/src/components/GameCard/index.js
@@ -1,8 +1,29 @@
 import { useState } from "react";
 import React from "react";
+import classNames from "classnames";
+import { useUserStore } from "stores/useUserStore";
 
+
+const GRADIENTS_LIGHT = {
+    "Voleibol 4x4": "bg-gradient-to-r from-[#f6d5f7] to-[#fbe9d7]",
+};
+
+const GRADIENTS_DARK = {
+    Atletismo: "bg-gradient-to-r from-[#b429f9] to-[#26c5f3]",
+    Andebol: "bg-gradient-to-r from-[#ff930f] to-[#fff95b]",
+    Basquetebol: "bg-gradient-to-r from-[#ce653b] to-[#2b0948]",
+    Futsal: "bg-gradient-to-r from-[#D22730] to-[#D19B28]",
+    "Futebol 7": "bg-gradient-to-r from-[#0f971c] to-[#0c0c0c]",
+    "Voleibol 4x4": "bg-gradient-to-r from-[#EF47FC] to-[#F3E2E2]",
+    "E-Sports LOL": "bg-gradient-to-r from-[#C29A3F] to-[#138D9B]",
+    "E-Sports CS:GO": "bg-gradient-to-r from-[#BB460C] to-[#E9B75B]",
+};
 
 const GameCard = ({ props }) => {
+    const theme = useUserStore((state) => state.theme);
+
+    const gradients = theme === "light" ? GRADIENTS_LIGHT : GRADIENTS_DARK;
+    const gradient = gradients[props.sport] ?? "";
 
     let time;
     if (props.live) {
@@ -17,38 +38,39 @@ const GameCard = ({ props }) => {
     const [isShown2, setIsShown2] = useState(false);
 
     return (
-        <div class="card h-70 max-w-lg bg-[#262626]">
-            <div class="grid grid-cols-2 pt-4 pr-4 pl-4 text-white font-bold">
-                <p>{props.modalidade}</p>
-                <p class="text-right">Grupo  A | Jornada 1</p>
+        <div class="card h-70 max-w-lg bg-base-300 group  rounded-b-[20px]">
+            <div class="grid grid-cols-2 pt-4 pr-4 pl-4 text-base-content font-bold">
+                <p>{props.sport} {props.type}</p>
+                <p class="text-right">Grupo  {props.Group} | Jornada {props.jornada}</p>
             </div>
             <div class="grid grid-cols-3 pt-4 pb-1 text-md">
                 <p className="pl-4">PAH</p>
-                <p className="place-self-center">31 Março</p>
+                <p className="place-self-center">{props.date}</p>
                 {time}
             </div>
-            <div className="rounded-b-[20px] grid grid-cols-3 bg-[#1F1F1F] pt-4">
-                <div>
-                    <div className="flex justify-center">
-                        <img onMouseEnter={() => setIsShown(true)}
-                            onMouseLeave={() => setIsShown(false)} className="max-h-[110px] transition hover:-translate-y-4 hover:max-h-[80px]" src={props.img2} />
-                    </div>
+            <div
+                className={`relative text-blue-400 border-none brightness-75 group-hover:brightness-110 h-1 ${gradient} transition-[filter] transition-duration-400`}
+            >
+                <div
+                    className={`absolute opacity-0 -inset-[1px] blur-lg ${gradient} group-hover:opacity-100 brightness-110 transition-opacity transition-duration-400 `}
+                ></div>
+            </div>
+            <div className="rounded-b-[20px] grid grid-cols-3 bg-base-200 pt-4">
+                <div onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)} className="flex flex-col justify-center">
+                    <img className={classNames('max-h-[110px] transition ', { '-translate-y-4 max-h-[80px]': isShown })} src={props.img1} />
                     {isShown && (
-                        <div className="text-center transition -translate-y-8 font-bold text-lg">Gestao comercial</div>
+                        <div className="text-center transition -translate-y-8 font-bold text-lg">{props.team1}</div>
                     )}
                 </div>
-                <div>
-                    <p className="text-center text-4xl text-white font-bold">{props.score1} - {props.score2}</p>
+                <div className="text-base-content">
+                    <p className="text-center text-4xl font-bold">{props.score1} - {props.score2}</p>
                     <p className="text-center text-2xl font-bold">({props.penalti1}) - ({props.penalti2})</p>
                 </div>
-                <div>
-                    <div onMouseEnter={() => setIsShown2(true)}
-                            onMouseLeave={() => setIsShown2(false)} className="flex flex-col justify-center">
-                        <img  className="max-h-[110px] transition hover:-translate-y-4 hover:max-h-[80px]" src={props.img2} />
+                <div onMouseEnter={() => setIsShown2(true)} onMouseLeave={() => setIsShown2(false)} className="flex flex-col justify-center">
+                    <img className={classNames('max-h-[110px] transition ', { '-translate-y-4 max-h-[80px]': isShown2 })} src={props.img2} />
                     {isShown2 && (
-                        <div className="text-center transition -translate-y-8 font-bold text-lg">Tecnol. Informaçao</div>
+                        <div className="text-center transition -translate-y-8 font-bold text-lg">{props.team2}</div>
                     )}
-                    </div>
                 </div>
             </div>
         </div>
