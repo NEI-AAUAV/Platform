f0e7b8050f2444fe7f5708fed6c493446ae39c1c
Blosuhm <zakhar.kruptsala@gmail.com>
2023-05-16 12:14:48 +0100

    feat(gala): add icons to Navbar
    - installed fontawesome
    - added chair and vote icons

--- extensions/gala/web-gala/src/components/Navbar/Navigation.tsx
+++ extensions/gala/web-gala/src/components/Navbar/Navigation.tsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChair, faCheckToSlot } from "@fortawesome/free-solid-svg-icons";
 
 type NavigationProps = {
   className?: string;
@@ -7,12 +9,16 @@ type NavigationProps = {
 export default function Navigation({ className }: NavigationProps) {
   return (
     <nav className={className}>
-      <ul className="flex flex-col gap-3 md:gap-6 sm:flex-row font-light">
+      <ul className="flex flex-col gap-4 md:gap-8 sm:flex-row">
         <li>
-          <Link to="/tables">Reservar Lugar</Link>
+          <Link to="/tables">
+            <FontAwesomeIcon icon={faChair} /> Reservar Lugar
+          </Link>
         </li>
         <li>
-          <Link to="/vote">Votar</Link>
+          <Link to="/vote">
+            <FontAwesomeIcon icon={faCheckToSlot} /> Votar
+          </Link>
         </li>
       </ul>
     </nav>
