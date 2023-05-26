ecb5c0a9ed9bb58ad63bd9913d3dab47f0022ffd
Blosuhm <zakhar.kruptsala@gmail.com>
2023-05-26 16:20:14 +0100

    feat(gala): created guest list component

--- extensions/gala/web-gala/src/components/TableModal/index.tsx
+++ extensions/gala/web-gala/src/components/TableModal/index.tsx
@@ -1,4 +1,7 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import Table from "@/components/Table";
+import GuestList from "./GuestList";
 
 type TableModalProps = {
   table: Table;
@@ -6,9 +9,24 @@ type TableModalProps = {
 };
 
 export default function TableModal({ table, className }: TableModalProps) {
+  const type = {
+    free: <>ola</>,
+    occupied: <>ola</>,
+    owner: <>ola</>,
+    guest: <>ola</>,
+  };
+
   return (
-    <div className={`bg-base-100 rounded-3xl ${className}`}>
+    <div className={`rounded-3xl bg-base-100 p-4 shadow-lg  ${className}`}>
       <Table table={table} />
+      <h3 className="text-xl font-semibold">Convidad@s</h3>
+      <GuestList persons={table.persons} />
+      <button
+        type="button"
+        className="rounded-3xl bg-gradient-to-tr from-dark-gold to-light-gold px-4 py-2 font-semibold"
+      >
+        <FontAwesomeIcon icon={faPaperPlane} /> Enviar Convite
+      </button>
     </div>
   );
 }
