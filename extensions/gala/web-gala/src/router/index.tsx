516d4c1c41f441b2016fefc7f23d8733f0ef8040
leand12 <leandrosilva12@ua.pt>
2023-05-29 02:25:16 +0100

    added authentication and service to web-gala

--- extensions/gala/web-gala/src/router/index.tsx
+++ extensions/gala/web-gala/src/router/index.tsx
@@ -1,10 +1,16 @@
+import { useEffect } from "react";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import routes from "./routes";
+import { refreshToken } from "src/services/client";
 
 export default function Router() {
   const router = createBrowserRouter(routes, {
     basename: "/gala",
   });
+
+  useEffect(() => {
+    refreshToken();
+  }, []);
 
   return <RouterProvider router={router} />;
 }
---
+++ extensions/gala/web-gala/src/services/GalaService.js
@@ -0,0 +1,15 @@
+import { createClient } from "./client";
+
+const client = createClient("http://localhost/api/gala/v1");
+
+class GalaService {
+  async listTables() {
+    return await client.get("/table/list");
+  }
+
+  async getTable(id) {
+    return await client.get(`/table/${id}`);
+  }
+}
+
+export default new GalaService();
