9a5ef5f5ca67439b95d55c1a178e2f5808cbbb15
Blosuhm <zakhar.kruptsala@gmail.com>
2023-05-31 23:57:32 +0100

    feat?(gala): utter chaos in reserve page
    - fixed testing tables to follow the type
    - don't ask me, look at the changes, as I said earlier utter chaos

--- extensions/gala/web-gala/src/pages/Reserve/index.tsx
+++ extensions/gala/web-gala/src/pages/Reserve/index.tsx
@@ -1,4 +1,4 @@
-import { Navigate, useNavigate, useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 import TableModal from "../TableModal";
 import Table from "@/components/Table";
@@ -7,7 +7,7 @@ const testingTables = [
   {
     _id: 0,
     name: "mesa1",
-    head: "João Beiros",
+    head: 0,
     seats: 8,
     persons: [
       {
@@ -34,30 +34,30 @@ const testingTables = [
   {
     _id: 1,
     name: "mesa2",
-    head: "John Doe",
+    head: 24434,
     seats: 10,
     persons: [
       {
         id: 0,
-        allergies: "",
-        dish: "Meat",
-        confirmed: true,
-        companions: [
-          {
-            dish: "NOR",
-            allergies: "",
-          },
-        ],
-      },
-      {
-        id: 1,
         allergies: "",
         dish: "NOR",
         confirmed: true,
         companions: [
           {
             dish: "NOR",
-            allergies: "",
+            allergies: "a",
+          },
+        ],
+      },
+      {
+        id: 1,
+        allergies: "",
+        dish: "VEG",
+        confirmed: true,
+        companions: [
+          {
+            dish: "VEG",
+            allergies: "a",
           },
           {
             dish: "NOR",
@@ -70,7 +70,7 @@ const testingTables = [
   {
     _id: 2,
     name: "mesa2",
-    head: "John Doe",
+    head: 264872487,
     seats: 10,
     persons: [
       {
@@ -106,7 +106,7 @@ const testingTables = [
   {
     _id: 3,
     name: "mesa2",
-    head: "John Doe",
+    head: 479849587,
     seats: 5,
     persons: [
       {
@@ -142,14 +142,14 @@ const testingTables = [
   {
     _id: 4,
     name: "mesa2",
-    head: "John Doe",
+    head: 948084096,
     seats: 10,
     persons: [],
   },
   {
     _id: 5,
     name: "mesa2",
-    head: "John Doe",
+    head: 843058904986,
     seats: 10,
     persons: [
       {
@@ -201,7 +201,7 @@ const testingTables = [
   {
     _id: 6,
     name: "mesa2",
-    head: "John Doe",
+    head: 5975070948,
     seats: 10,
     persons: [
       {
@@ -216,7 +216,7 @@ const testingTables = [
   {
     _id: 7,
     name: "mesa2",
-    head: "John Doe",
+    head: 9847698749687,
     seats: 10,
     persons: [
       {
@@ -252,7 +252,7 @@ const testingTables = [
   {
     _id: 8,
     name: "mesa2",
-    head: "John Doe",
+    head: 8379573987,
     seats: 10,
     persons: [
       {
@@ -287,27 +287,43 @@ const testingTables = [
   },
 ];
 
-function useTable(id: string | undefined) {
-  const [tableById, setTableById] = useState<Table>();
+function useTable() {
+  const [tableById, setTableById] = useState<Table | undefined>(undefined);
   const navigate = useNavigate();
+  const { tableId } = useParams();
   useEffect(() => {
-    if (id === undefined) return;
+    if (tableId === undefined) {
+      setTableById(undefined);
+      return;
+    }
     // TODO: fetch table from server and separate this hook
     const tableFromId = testingTables.find(
-      (table) => table._id === parseInt(id, 10),
+      (table) => table._id === parseInt(tableId, 10),
     );
     if (tableFromId === undefined) {
       navigate("/reserve");
     }
     setTableById(tableFromId);
-  }, [id]);
+  }, [tableId]);
 
   return tableById;
 }
 
 export default function Reserve() {
-  const { tableId } = useParams();
-  const tablePage = useTable(tableId);
+  const tablePage = useTable();
+
+  // TODO: Data fetching
+  // useEffect(() => {
+  //   const fetchTables = async () => {
+  //     const response = await GalaService.listTables();
+  //     const { data } = response.statusText;
+
+  //     setStatusN(details);
+  //     setTables(data);
+  //   };
+  //   fetchTables();
+  // }, []);
+
   return (
     <>
       <h2 className="m-20 text-center text-2xl font-bold">
@@ -315,7 +331,9 @@ export default function Reserve() {
       </h2>
       <div className="mx-10 grid grid-cols-[repeat(auto-fit,_minmax(13.25rem,_1fr))] gap-14">
         {testingTables.map((table) => (
-          <Table key={table._id} table={table} />
+          <Link to={`/reserve/${table._id}`}>
+            <Table table={table} />
+          </Link>
         ))}
       </div>
       {tablePage !== undefined && <TableModal table={tablePage} />}
