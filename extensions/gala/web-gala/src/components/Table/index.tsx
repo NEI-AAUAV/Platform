45a471cb64f8d476660914b60c427ef7db9f470b
Blosuhm <zakhar.kruptsala@gmail.com>
2023-06-06 13:38:19 +0100

    updated table to show owner's name and avatar

--- extensions/gala/web-gala/src/components/Table/index.tsx
+++ extensions/gala/web-gala/src/components/Table/index.tsx
@@ -1,17 +1,24 @@
+import Avatar from "../Avatar";
 import VisualTable from "./VisualTable";
+import useNEIUser from "@/hooks/useNEIUser";
 
 type TableProps = {
   table: Table;
   className?: string;
 };
 
-// TODO! Leandro se vires isto faz me o modal
 export default function Table({ table, className }: TableProps) {
   const { name, head } = table;
+  const { neiUser } = useNEIUser(head);
   return (
     <div className={`flex flex-col items-center ${className}`}>
-      <h4 className="z-10 text-xl font-semibold">{name}</h4>
-      <h6 className="z-10 mb-6 text-sm font-light uppercase">{head}</h6>
+      <h4 className="z-10 text-xl font-semibold">{name ?? "Mesa sem nome"}</h4>
+      <h6 className="z-10 mb-6 text-sm font-light uppercase">
+        {head !== null && <Avatar id={head} className="w-4" />}{" "}
+        {neiUser?.name === undefined || neiUser?.surname === undefined
+          ? "Sem nome"
+          : `${neiUser.name} ${neiUser.surname}`}
+      </h6>
       <VisualTable table={table} />
     </div>
   );
