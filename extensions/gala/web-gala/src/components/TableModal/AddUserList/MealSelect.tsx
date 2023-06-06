5f5923c3c1f9a1cf677337fc25dc59de688a1e26
Blosuhm <zakhar.kruptsala@gmail.com>
2023-06-06 13:35:30 +0100

    made Select component generic and updated MealSelect to use it

--- extensions/gala/web-gala/src/components/TableModal/AddUserList/MealSelect.tsx
+++ extensions/gala/web-gala/src/components/TableModal/AddUserList/MealSelect.tsx
@@ -1,7 +1,6 @@
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Listbox, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import Select from "@/components/Select";
 
 type MealSelectProps = {
   selected: string;
@@ -10,9 +9,9 @@ type MealSelectProps = {
   style?: React.CSSProperties;
 };
 
-const optionMap = new Map<string, string>([
-  ["NOR", "Carne"],
-  ["VEG", "Vegetariano"],
+const optionMap = new Map<string, JSX.Element>([
+  ["NOR", <>Carne</>],
+  ["VEG", <>Vegetariano</>],
 ]);
 
 export default function MealSelect({
@@ -21,39 +20,26 @@ export default function MealSelect({
   className,
   style,
 }: MealSelectProps) {
+  const options: [JSX.Element, string][] = [
+    [optionMap.get("NOR") ?? <>Error</>, "NOR"],
+    [optionMap.get("VEG") ?? <>Error</>, "VEG"],
+  ];
   return (
     <div className={`relative w-full ${className}`} style={style}>
-      <Listbox value={selected} onChange={setSelected}>
-        <Listbox.Button className="flex w-full items-center rounded-3xl bg-light-gold px-3 py-2 text-start">
-          {optionMap.get(selected)}
-          <FontAwesomeIcon
-            className="ml-auto ui-open:rotate-180"
-            icon={faCaretDown}
-          />
-        </Listbox.Button>
-        {/* [&>*] is the selector for all direct children */}
-        <Transition
-          as={Fragment}
-          enter="transition ease-out duration-100"
-          enterFrom="opacity-0"
-          enterTo="opacity-100"
-          leave="transition ease-in duration-100"
-          leaveFrom="opacity-100"
-          leaveTo="opacity-0"
-        >
-          <Listbox.Options className="absolute top-full z-10 mt-2 w-full overflow-visible rounded-2xl border border-light-gold bg-base-100 shadow-[0_4px_16px_rgba(0,_0,_0,_0.25)] [&>*]:cursor-pointer">
-            <Listbox.Option
-              className="border-b border-light-gold px-3 py-2"
-              value="NOR"
-            >
-              {optionMap.get("NOR")}
-            </Listbox.Option>
-            <Listbox.Option className="px-3 py-2" value="VEG">
-              {optionMap.get("VEG")}
-            </Listbox.Option>
-          </Listbox.Options>
-        </Transition>
-      </Listbox>
+      <Select
+        selected={selected}
+        setSelected={setSelected}
+        title={
+          <>
+            {optionMap.get(selected)}
+            <FontAwesomeIcon
+              className="ml-auto ui-open:rotate-180"
+              icon={faCaretDown}
+            />
+          </>
+        }
+        options={options}
+      />
     </div>
   );
 }
