f93d736bb6a5172ba1e5c13dab5c8d676afaed7f
leand12 <leandrosilva12@ua.pt>
2023-06-10 00:41:00 +0100

    fix erro do zakhar

--- extensions/gala/web-gala/src/hooks/userHooks/useUserEdit.ts
+++ extensions/gala/web-gala/src/hooks/userHooks/useUserEdit.ts
@@ -2,11 +2,7 @@ import GalaService from "@/services/GalaService";
 
 type EditUser = {
   id: number;
-  name: string;
-  email: string;
-  matriculation: number | null;
-  nmec: number | null;
-  has_payed?: boolean;
+  has_payed: boolean;
 };
 
 export default async function useUserEdit(request: EditUser) {
