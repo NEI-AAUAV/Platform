266a3bc3f67dc549315ecdf1566b93abc1976445
leand12 <leandrosilva12@ua.pt>
2023-06-09 05:10:04 +0100

    acredita

--- extensions/gala/web-gala/src/pages/Layout/index.tsx
+++ extensions/gala/web-gala/src/pages/Layout/index.tsx
@@ -4,21 +4,12 @@ import config from "@/config";
 
 const backgrounds = {
   home: {
-<<<<<<< HEAD
-    backgroundImage: "url('http://localhost/gala/home-background.png')",
-=======
-    backgroundImage: `url('${config.BASE_URL}/gala/public/home-background.png')`,
->>>>>>> cde63fcda5b774c8ea340efbf857ef0ede3d3a4d
+    backgroundImage: `url('${config.BASE_URL}/gala/home-background.png')`,
     backgroundSize: "cover",
     backgroundPosition: "center",
   },
   notHome: {
-<<<<<<< HEAD
-    backgroundImage:
-      "url('http://localhost/gala/not-home-background.png')",
-=======
-    backgroundImage: `url('${config.BASE_URL}/gala/public/not-home-background.png')`,
->>>>>>> cde63fcda5b774c8ea340efbf857ef0ede3d3a4d
+    backgroundImage: `url('${config.BASE_URL}/gala/not-home-background.png')`,
     backgroundSize: "100vh",
     backgroundPosition: "top left",
   },
