63617fe8eb364fba5bf41d08a95c181aa5c8e9e1
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2023-06-22 02:40:13 +0100

    Vote page with placeholder cards

--- extensions/gala/web-gala/src/pages/Vote/index.tsx
+++ extensions/gala/web-gala/src/pages/Vote/index.tsx
@@ -1,7 +1,24 @@
+import classNames from "classnames";
+import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "@/components/Button";
+import VoteCard from "@/components/VoteCard";
+
 export default function Vote() {
   return (
-    <h1 className="absolute inset-0 flex items-center justify-center text-center text-5xl sm:text-7xl">
-      Em breve
-    </h1>
+    <>
+      <h2 className="m-20 text-center text-2xl font-bold">
+        <span className="block">Título</span>
+      </h2>
+      <div className="mx-4 my-10 grid gap-8">
+        <VoteCard />
+        <VoteCard />
+        <VoteCard />
+
+        <Button className={classNames("w-full")} submit>
+          <FontAwesomeIcon icon={faPaperPlane} /> Enviar Convite
+        </Button>
+      </div>
+    </>
   );
 }
