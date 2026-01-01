d9729fb028ec4ec6e2d32d4c70dda0413ddbb908
Martim Gil <martimgil@ua.pt>
2026-01-01 11:39:16 +0000

    fix(api-family): use JSONResponse in tests instead of ORJSONResponse

--- api-family/app/tests/conftest.py
+++ api-family/app/tests/conftest.py
@@ -3,7 +3,7 @@ from typing import Generator, Any
 
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
-from fastapi.responses import ORJSONResponse
+from fastapi.responses import JSONResponse
 
 from app.api.v1 import api_v1_router
 from app.core.config import settings
@@ -13,7 +13,7 @@ from app.core.config import settings
 def app() -> Generator[FastAPI, Any, None]:
     """Create a new application for the test session."""
 
-    _app = FastAPI(default_response_class=ORJSONResponse)
+    _app = FastAPI(default_response_class=JSONResponse)
     _app.include_router(api_v1_router, prefix=settings.API_V1_STR)
     yield _app
 
